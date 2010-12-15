fb98fde48a0d5ce30498dc2986a7054b318fed2a
mihai_radulescu <mopuffus@yahoo.com>
2010-12-15 19:30:26 +0100

    try 1 for the RunAsInterceptor

---
+++ phrs-modules/security/src/main/java/at/srfg/kmt/ehealth/phrs/security/impl/RunAsConstants.java
@@ -0,0 +1,18 @@
+/*
+ * Project :iCardea
+ * File : RunAsConstants.java
+ * Date : Dec 15, 2010
+ * User : mradules
+ */
+
+package at.srfg.kmt.ehealth.phrs.security.impl;
+
+/**
+ *
+ * @author mradules
+ */
+enum RunAsConstants {
+
+    ROLES, GROUPS;
+
+}
---
+++ phrs-modules/security/src/main/java/at/srfg/kmt/ehealth/phrs/security/impl/RunAsInterceptor.java
@@ -0,0 +1,62 @@
+/*
+ * Project :iCardea
+ * File : RunAsInterceptor.java
+ * Date : Dec 15, 2010
+ * User : mradules
+ */
+
+package at.srfg.kmt.ehealth.phrs.security.impl;
+
+
+import static at.srfg.kmt.ehealth.phrs.security.impl.RunAsConstants.GROUPS;
+import static at.srfg.kmt.ehealth.phrs.security.impl.RunAsConstants.ROLES;
+import at.srfg.kmt.ehealth.phrs.security.api.RunAs;
+import at.srfg.kmt.ehealth.phrs.security.api.RunAsGroup;
+import at.srfg.kmt.ehealth.phrs.security.api.RunAsRole;
+import at.srfg.kmt.ehealth.phrs.security.model.Group;
+import at.srfg.kmt.ehealth.phrs.security.model.Role;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+import javax.interceptor.AroundInvoke;
+import javax.interceptor.InvocationContext;
+
+/**
+ *
+ * @author Mihai
+ */
+public class RunAsInterceptor {
+
+    @AroundInvoke
+    public Object intercept(InvocationContext context) throws Exception {
+        final Object target = context.getTarget();
+        final RunAs annotation =
+                    target.getClass().getAnnotation(RunAs.class);
+        boolean isRunAs = annotation != null;
+        if (!isRunAs) {
+            return context.proceed();
+        }
+
+        // FIXME : instance of is expensive, prove using the class
+        final boolean isRunAsGroup = target instanceof RunAsGroup;
+        final Set<Group> groups = isRunAsGroup
+                ? ((RunAsGroup) target).getGroups()
+                : new HashSet<Group>();
+
+        // FIXME : instance of is expensive, prove using the class
+        final boolean isAsRole = target instanceof RunAsRole;
+        final Set<Role> roles = isAsRole
+                ? ((RunAsRole) target).getRoles()
+                : new HashSet<Role>();
+
+        final Map<String, Object> contextData = context.getContextData();
+        if (!groups.isEmpty()) {
+            contextData.put(GROUPS.toString(), groups);
+        }
+        if (!roles.isEmpty()) {
+            contextData.put(ROLES.toString(), roles);
+        }
+
+        return context.proceed();
+    }
+}
