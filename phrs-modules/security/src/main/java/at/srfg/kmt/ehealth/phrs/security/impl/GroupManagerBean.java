f7ffcf8d3644647a5f646783bf407cf84053cd96
mihai_radulescu <mopuffus@yahoo.com>
2010-12-15 19:11:17 +0100

    I am tired and I start to hear voices, I need some food and rest

--- phrs-modules/security/src/main/java/at/srfg/kmt/ehealth/phrs/security/impl/GroupManagerBean.java
+++ phrs-modules/security/src/main/java/at/srfg/kmt/ehealth/phrs/security/impl/GroupManagerBean.java
@@ -5,7 +5,9 @@
  * User : mradules
  */
 
+
 package at.srfg.kmt.ehealth.phrs.security.impl;
+
 
 import at.srfg.kmt.ehealth.phrs.security.api.GroupManager;
 import at.srfg.kmt.ehealth.phrs.security.model.Group;
@@ -15,7 +17,12 @@ import javax.ejb.Local;
 
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
+import javax.persistence.NoResultException;
+import javax.persistence.NonUniqueResultException;
 import javax.persistence.PersistenceContext;
+import javax.persistence.Query;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 /**
  *
@@ -25,27 +32,172 @@ import javax.persistence.PersistenceContext;
 @Local(GroupManager.class)
 public class GroupManagerBean implements GroupManager {
 
+    /**
+     * The Logger instance. All log messages from this class
+     * are routed through this member. The Logger name space
+     * is <code>at.srfg.kmt.ehealth.phrs.security.impl.GroupManager</code>.
+     */
+    private static final Logger logger =
+            LoggerFactory.getLogger(GroupManagerBean.class);
+
+    /**
+     * Used to communicate with the underlying persistence layer.
+     */
     @PersistenceContext
     private EntityManager entityManager;
 
+    /**
+     * Registers a  group instance or update and existing one if 
+     * the underlying persistence contains already a group with
+     * the same name. The method returns true if the group is
+     * added and false if the group is updated.
+     *
+     * @param group the group to add, it can not be null.
+     * @return true if the group is added, false if the group is updated.
+     * @throws NullPointerException if the <code>group</code> argument is null.
+     * @throws GroupException if the underlying persistence layer contains more
+     * than one group with the same name. The exception transports the
+     * <code>Group</code> that cause the exception, this can be obtained by
+     * calling the <code>getGroup</code> method (from the GroupException).
+     * @see GroupException
+     */
     @Override
     public boolean addGroup(Group group) {
-        throw new UnsupportedOperationException("Not supported yet.");
+
+        if (group == null) {
+            final NullPointerException nullException =
+                    new NullPointerException("The Group argument can not be null.");
+            logger.error(nullException.getMessage(), nullException);
+            throw nullException;
+        }
+
+        logger.debug("Tries add Group [#0]", group);
+        final String name = group.getName();
+
+        final Group oldGroup;
+        try {
+            oldGroup = getGroupForName(name);
+        } catch (NonUniqueResultException exception) {
+            final GroupException gException =
+                    new GroupException(exception);
+            gException.setGroup(group);
+            logger.error("Duplicate group found for #0", group);
+            logger.error(gException.getMessage(), exception);
+            throw gException;
+        }
+
+        if (oldGroup == null) {
+            entityManager.persist(group);
+            logger.debug("Group [#0] was persisted.", group);
+            return true;
+        }
+
+        final String newName = group.getName();
+        if (newName != null && !newName.isEmpty()) {
+            oldGroup.setName(newName);
+        }
+
+        final String newDescription = group.getDescription();
+        if (newDescription != null && !newDescription.isEmpty()) {
+            oldGroup.setDescription(newDescription);
+        }
+
+        final Set<User> newUsers = group.getUsers();
+        if (newUsers != null) {
+            // FIXME : I think I must remove the old one as well
+            oldGroup.setUsers(newUsers);
+        }
+
+        logger.debug("Group [#0] was upadted.", group);
+        return false;
+    }
+
+    /**
+     * Searches a group with the given name.
+     *
+     * @param name the name for the group.
+     * @return the group with the given name.
+     * @throws NonUniqueResultException if the underlying persistence
+     * later contains more groups with this name (this lead to an inconsistent
+     * database model).
+     * @throws NullPointerException if the <code>group</code> argument is null.
+     * @throws GroupException if the underlying persistence layer contains more
+     * than one group with the same name. The exception transports the
+     * <code>Group</code> that cause the exception, this can be obtained by
+     * calling the <code>getGroup</code> method (from the GroupException).
+     * @see GroupException
+     */
+    private Group getGroupForName(String name) {
+        final Query query = entityManager.createNamedQuery("findGroupForName");
+        query.setParameter("name", name);
+
+        try {
+            final Group result = (Group) query.getSingleResult();
+            return result;
+        } catch (NoResultException exception) {
+            logger.debug("No group with the name [#0] found.", name);
+            return null;
+        }
+    }
+
+    /**
+     * Proves if the underlying persistence contains a given group.
+     *
+     * @param group the group which the existence is to be tested.
+     * @return true if the underlying persistence contains a given
+     * group, false other wise.
+     */
+    @Override
+    public boolean groupExist(Group group) {
+
+        if (group == null) {
+            final NullPointerException nullException =
+                    new NullPointerException("The Group argument can not be null.");
+            logger.error(nullException.getMessage(), nullException);
+            throw nullException;
+        }
+
+
+        final String name = group.getName();
+        try {
+            final Group oldGroup = getGroupForName(name);
+            return oldGroup != null;
+        } catch (NonUniqueResultException exception) {
+            final GroupException gException =
+                    new GroupException(exception);
+            gException.setGroup(group);
+            logger.error("Duplicate group found for #0", group);
+            logger.error(gException.getMessage(), exception);
+            throw gException;
+        }
+
     }
 
     @Override
-    public boolean groupExist(Group principal) {
-        throw new UnsupportedOperationException("Not supported yet.");
-    }
+    public Group removeGroup(Group group) {
+        
+        if (group == null) {
+            final NullPointerException nullException =
+                    new NullPointerException("The Group argument can not be null.");
+            logger.error(nullException.getMessage(), nullException);
+            throw nullException;
+        }
 
-    @Override
-    public Group removeGroup(Group principal) {
-        throw new UnsupportedOperationException("Not supported yet.");
+        final String name = group.getName();
+        // mihai : I can also do a 'merge' to assign the entity to the
+        // actaul context.
+        final Group oldGroup = getGroupForName(name);
+        if (oldGroup != null) {
+            entityManager.remove(oldGroup);
+        }
+
+        return oldGroup;
     }
 
     @Override
     public void removeAllGroups() {
-        throw new UnsupportedOperationException("Not supported yet.");
+        final Query query = entityManager.createNamedQuery("removeAllGroups");
+        query.executeUpdate();
     }
 
     @Override
--- phrs-modules/security/src/main/java/at/srfg/kmt/ehealth/phrs/security/impl/PermissionManagerBean.java
+++ phrs-modules/security/src/main/java/at/srfg/kmt/ehealth/phrs/security/impl/PermissionManagerBean.java
@@ -7,10 +7,62 @@
 
 package at.srfg.kmt.ehealth.phrs.security.impl;
 
+import at.srfg.kmt.ehealth.phrs.security.api.Action;
+import at.srfg.kmt.ehealth.phrs.security.api.Item;
+import at.srfg.kmt.ehealth.phrs.security.api.PermissionManager;
+import at.srfg.kmt.ehealth.phrs.security.model.Group;
+import at.srfg.kmt.ehealth.phrs.security.model.Role;
+import at.srfg.kmt.ehealth.phrs.security.model.User;
+import javax.ejb.Local;
+import javax.ejb.Stateless;
+
 /**
  *
  * @author Mihai
  */
-public class PermissionManagerBean {
+
+@Stateless
+@Local(PermissionManager.class)
+public class PermissionManagerBean implements PermissionManager {
+
+    @Override
+    public Group getAllowdGroupFor(Action action, Item item) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public Role getAllowdRoleFor(Action action, Item item) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public boolean prove(User user, Action action, Item item) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public boolean prove(User user, Action action) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public boolean prove(Role role, Action action, Item item) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public boolean prove(Role role, Action action) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public boolean prove(Group role, Action action, Item item) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
+
+    @Override
+    public boolean prove(Group role, Action action) {
+        throw new UnsupportedOperationException("Not supported yet.");
+    }
 
 }
