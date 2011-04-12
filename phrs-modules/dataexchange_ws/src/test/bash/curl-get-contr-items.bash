3b4789a9660ff6b573a8449f8821f0fbd134bf11
mihai <mihai@dyr>
2011-04-12 10:30:49 +0200

    More documentation

--- phrs-modules/dataexchange_ws/src/test/bash/curl-get-contr-items.bash
+++ phrs-modules/dataexchange_ws/src/test/bash/curl-get-contr-items.bash
@@ -4,5 +4,5 @@
 HOST=http://localhost:8080
 CONTEXT=dataexchange_ws/controlled_item_repository
 
-#curl --fail  -X GET $HOST/$CONTEXT/get?q=2.16.840.1.113883.6.96:19019007
+#2.16.840.1.113883.6.96:19019007 - the syntax is code system code : item code
 curl --fail  -X GET $HOST/$CONTEXT/get?q=2.16.840.1.113883.6.96
