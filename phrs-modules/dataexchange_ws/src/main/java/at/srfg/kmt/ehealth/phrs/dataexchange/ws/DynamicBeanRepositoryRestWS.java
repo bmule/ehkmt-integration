1f07ea521c62722e16cf8506c805deb5d9589bb5
mihai <mihai@dyr>
2011-04-12 12:33:35 +0200

    The very first try for the Class/Bean repository WS.

---
+++ phrs-modules/dataexchange_ws/src/main/java/at/srfg/kmt/ehealth/phrs/dataexchange/ws/DynamicBeanRepositoryRestWS.java
@@ -0,0 +1,123 @@
+/*
+ * Project :iCardea
+ * File : DynamicBeanRepositoryRestWS.java
+ * Encoding : UTF-8
+ * Date : Mar 22, 2011
+ * User : Mihai Radulescu
+ */
+package at.srfg.kmt.ehealth.phrs.dataexchange.ws;
+
+import at.srfg.kmt.ehealth.phrs.dataexchange.api.DynamicBeanRepository;
+import at.srfg.kmt.ehealth.phrs.dataexchange.api.DynamicClassRepository;
+import at.srfg.kmt.ehealth.phrs.util.JBossJNDILookup;
+import javax.naming.NamingException;
+import javax.ws.rs.FormParam;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.Response.Status;
+import net.sf.json.JSONObject;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Provides web services for the <code>DynamicBeanRepository</code>. </br>
+ * This class exposes :
+ * <ul>
+ * <li> <JBOSS URI>/dataexchange_ws/dynamic_bean_repository/persist
+ * <li> <JBOSS URI>/dataexchange_ws/dynamic_bean_repository/getAllForClass
+ * <li> <JBOSS URI>/dataexchange_ws/dynamic_bean_repository/getLastForClass
+ * </ul>
+ *
+ * @author Mihai
+ */
+@Path("/dynamic_bean_repository")
+public class DynamicBeanRepositoryRestWS {
+
+    public static final String CLASS_URI_PROPERTY = "class_uri";
+    /**
+     * The Logger instance. All log messages from this class
+     * are routed through this member. The Logger name space
+     * is <code>at.srfg.kmt.ehealth.phrs.security.impl.DynamicBeanRepositoryRestWS</code>.
+     */
+    private static final Logger LOGGER =
+            LoggerFactory.getLogger(DynamicBeanRepositoryRestWS.class);
+
+    @GET
+    @Path("/test")
+    @Produces("application/json")
+    public Response test() {
+        return Response.status(Status.OK).build();
+    }
+
+    /**
+     * POST based web service used to persist a given dynamic bean JSON 
+     * serialized. </br>
+     * This web service can be access on :  
+     * <JBOSS URI>/dataexchange_ws/dynamic_bean_repository/persist
+     * 
+     * @return <code>javax.ws.rs.core.Response.Status.OK</code> if the operation 
+     * is successfully or <code>javax.ws.rs.core.Response.Status.INTERNAL_SERVER_ERROR</code> 
+     * by any kind of error.
+     */
+    @POST
+    @Path("/persist")
+    @Produces("application/json")
+    public Response persist(@FormParam("dynaBean") String dynaBean) {
+        LOGGER.debug("Tries to persists {}", dynaBean);
+        final JSONObject json;
+        try {
+            json = JSONObject.fromObject(dynaBean);
+        } catch (RuntimeException rte) {
+            LOGGER.error("This dynabean JSON representation [{}] is not valid", dynaBean);
+            LOGGER.error(rte.getMessage(), rte);
+            return Response.status(Status.BAD_REQUEST).build();
+        }
+
+        final Object classURI = json.get(CLASS_URI_PROPERTY);
+        if (classURI == null) {
+            LOGGER.error("This dynabean JSON representation [{}] does not have a class_uri property", dynaBean);
+            return Response.status(Status.BAD_REQUEST).build();
+        }
+
+        final DynamicClassRepository classRepository;
+        try {
+            classRepository = JBossJNDILookup.lookupLocal(DynamicClassRepository.class);
+        } catch (NamingException namingException) {
+            LOGGER.error(namingException.getMessage(), namingException);
+            return Response.status(Status.INTERNAL_SERVER_ERROR).build();
+        }
+        
+        final boolean classExits = classRepository.exits(classURI.toString());
+        if (!classExits) {
+            LOGGER.error("The class with URI [{}] is not register in the repository.", classURI);
+            return Response.status(Status.BAD_REQUEST).build();
+        }
+
+        final DynamicBeanRepository beanRepository;
+        try {
+            beanRepository = JBossJNDILookup.lookupLocal(DynamicBeanRepository.class);
+        } catch (NamingException namingException) {
+            LOGGER.error(namingException.getMessage(), namingException);
+            return Response.status(Status.INTERNAL_SERVER_ERROR).build();
+        }
+
+        return Response.status(Status.OK).build();
+    }
+
+    @GET
+    @Path("/getAllForClass")
+    @Produces("application/json")
+    public Response getAllForClass() {
+        return Response.status(Status.OK).build();
+    }
+
+    @GET
+    @Path("/getLastForClass")
+    @Produces("application/json")
+    public Response getLastForClass() {
+        return Response.status(Status.OK).build();
+    }
+}
---
+++ phrs-modules/dataexchange_ws/src/main/java/at/srfg/kmt/ehealth/phrs/dataexchange/ws/DynamicClassRepositoryRestWS.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package at.srfg.kmt.ehealth.phrs.dataexchange.ws;
+
+/**
+ *
+ * @author mihai
+ */
+public class DynamicClassRepositoryRestWS {
+    
+}
