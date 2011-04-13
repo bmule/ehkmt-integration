ce3d4cc9a4516a4f8b1411b373cc188ce9ed203b
mihai_radulescu <mopuffus@yahoo.com>
2011-04-13 14:16:51 +0200

    the very first try

--- phrs-modules/dataexchange_ws/src/test/bash/curl-get-contr-items.bash
+++ phrs-modules/dataexchange_ws/src/test/bash/curl-get-contr-items.bash
@@ -4,5 +4,6 @@
 HOST=http://localhost:8080
 CONTEXT=dataexchange_ws/controlled_item_repository
 
+# this will extracts all the items with a certain code system.
 #2.16.840.1.113883.6.96:19019007 - the syntax is code system code : item code
 curl --fail  -X GET $HOST/$CONTEXT/get?q=2.16.840.1.113883.6.96
