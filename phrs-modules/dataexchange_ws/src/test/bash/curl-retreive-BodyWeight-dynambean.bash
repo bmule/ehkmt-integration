6aeb2bdc864a19359887f60310ce81448f4f3bd3
mihai_radulescu <mopuffus@yahoo.com>
2011-04-13 14:44:33 +0200

    more comments

--- phrs-modules/dataexchange_ws/src/test/bash/curl-retreive-BodyWeight-dynambean.bash
+++ phrs-modules/dataexchange_ws/src/test/bash/curl-retreive-BodyWeight-dynambean.bash
@@ -4,6 +4,6 @@
 HOST=http://localhost:8080
 CONTEXT=dataexchange_ws/dynamic_bean_repository
 
-# this is the BodyWeight
+# this is the here I retreive the last version for a a bean with class ...BodyWeight
 curl --fail  -X GET $HOST/$CONTEXT/getLastForClass?class_uri=at.srfg.kmt.ehealth.phrs.datamodel.impl.BodyWeight
  
