eec88c4dae46b03ad3bb00123ab64a3a37963a2d
mihai_radulescu <mopuffus@yahoo.com>
2011-04-14 09:06:18 +0200

    the very first try

---
+++ phrs-modules/dataexchange_ws/src/main/java/at/srfg/kmt/ehealth/phrs/dataexchange/model/package-info.java
@@ -0,0 +1,12 @@
+/*
+ * Project :iCardea
+ * File : package-info.java 
+ * Encoding : UTF-8
+ * Date : Apr 14, 2011
+ * User : Mihai Radulescu
+ */
+/**
+ * Contains all the classed for used to define PHRS models. The PHRS models are 
+ * based on dynamic classes / dynamic beans.
+ */
+package at.srfg.kmt.ehealth.phrs.dataexchange.model;
