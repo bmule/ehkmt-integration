c5c3c4bbe06fee539c1c3b8f60a71a8728125ad6
mihai_radulescu <mopuffus@yahoo.com>
2011-04-15 16:56:46 +0200

    the very fisrt try

---
+++ phrs-modules/dataexchange_ws/src/test/bash/curl-pull-BloodPreasure.bash
@@ -0,0 +1,9 @@
+#!/bin/bash
+#set -xv
+
+HOST=http://localhost:8080
+CONTEXT=dataexchange_ws/dynamic_bean_repository
+
+# phrsBeanClassURI : at.srfg.kmt.ehealth.phrs.datamodel.impl.BodyWeight
+curl --fail  -X GET $HOST/$CONTEXT/getLastForClass?class_uri=at.srfg.kmt.ehealth.phrs.datamodel.impl.BodyWeight
+ 
