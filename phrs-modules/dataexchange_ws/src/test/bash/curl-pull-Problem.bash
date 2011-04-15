9d1a62b41717adb98aa451b67ec546be93ad8c67
mihai_radulescu <mopuffus@yahoo.com>
2011-04-15 17:07:00 +0200

    typo is now out

--- phrs-modules/dataexchange_ws/src/test/bash/curl-pull-Problem.bash
+++ phrs-modules/dataexchange_ws/src/test/bash/curl-pull-Problem.bash
@@ -4,6 +4,6 @@
 HOST=http://localhost:8080
 CONTEXT=dataexchange_ws/dynamic_bean_repository
 
-# phrsBeanClassURI : at.srfg.kmt.ehealth.phrs.datamodel.impl.Probmel
-curl --fail  -X GET $HOST/$CONTEXT/getLastForClass?class_uri=at.srfg.kmt.ehealth.phrs.datamodel.impl.Probmel
+# phrsBeanClassURI : at.srfg.kmt.ehealth.phrs.datamodel.impl.Problem
+curl --fail  -X GET $HOST/$CONTEXT/getLastForClass?class_uri=at.srfg.kmt.ehealth.phrs.datamodel.impl.Problem
  
