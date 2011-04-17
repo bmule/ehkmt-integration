c9adaa1d5e1d78192fc3bb6f626da129f604d28f
mihai_radulescu <mopuffus@yahoo.com>
2011-04-17 17:12:32 +0200

    more comments.

--- phrs-modules/pcc10WS/src/test/java/at/srfg/kmt/ehealth/phrs/pcc10ws/impl/QUPCAR004030UVWebServiceExample.java
+++ phrs-modules/pcc10WS/src/test/java/at/srfg/kmt/ehealth/phrs/pcc10ws/impl/QUPCAR004030UVWebServiceExample.java
@@ -12,7 +12,7 @@
 package at.srfg.kmt.ehealth.phrs.pcc10ws.impl;
 
 
-import java.io.IOException;
+import at.srfg.kmt.ehealth.phrs.pcc10ws.api.PCC10BuildException;
 import java.util.Map;
 import javax.xml.bind.JAXBException;
 import javax.xml.ws.BindingProvider;
@@ -20,13 +20,13 @@ import org.hl7.v3.*;
 
 
 /**
- * This runnable class send an empty PCC9 query to :
- * http://127.0.0.1:8080/pcc10ws/QUPCAR004030UV_Service. <br>
+ * This runnable class send an empty PCC10 query to :
+ * http://127.0.0.1:8080/pcc10ws/QUPCAR004030UV_Service. <.br>
  * <b>Note : </b> This class required a running JBoss instance 
  * whit the PCC10 module installed. </br>
- * This is an example - it must be used only for didactical purposes. <br>
+ * This is an example - it must be used only for didactical purposes. </br>
  * To use this class from maven environment use :
- * mvn exec:java -Dexec.mainClass=at.srfg.kmt.ehealth.phrs.pcc09ws.impl.QUPCAR004030UVWebServiceExample -Dexec.classpathScope=test<br>
+ * mvn exec:java -Dexec.mainClass=at.srfg.kmt.ehealth.phrs.pcc10ws.impl.QUPCAR004030UVWebServiceExample -Dexec.classpathScope=test</br>
  * Take care the exec  does not compile the classes.
  * 
  * @version 0.1
@@ -36,33 +36,41 @@ import org.hl7.v3.*;
 public class QUPCAR004030UVWebServiceExample {
 
     /**
-     * Sends an empty PCC10 query to :
-     * http://127.0.0.1:8080/pcc10ws/QUPCAR004030UV_Service
+     * Builds and sends an empty PCC10 query to :
+     * http://127.0.0.1:8080/pcc10ws/QUPCAR004030UV_Service.
      * 
-     * @param args no arguments required
-     * @throws JAXBException 
+     * @param args no arguments required.
+     * @throws PCC10BuildException by if the request can not be properly 
+     * builded. 
      */
-    public static void main(String... args) throws JAXBException, IOException {
+    public static void main(String... args) throws PCC10BuildException {
         final QUPCAR004030UVService service =
                 new QUPCAR004030UVService();
 
-        // here I obtain the service.
+        // here I obtain the service (proxy).
         final QUPCAR004030UVPortType portType = service.getQUPCAR004030UVPort();
-
-        final QUPCIN043200UV01 response = QueryFactory.buildQUPCIN043200UV01();
+        final DefaultPCC10RequestFactory requestFactory = 
+                new DefaultPCC10RequestFactory();
+        final QUPCIN043200UV01 buildQUPCIN043200UV01 = requestFactory.build();
 
         // I set the end point for the 
         setDefaultEndPointURI(portType);
 
         final MCCIIN000002UV01 ack =
-                portType.qupcAR004030UVQUPCIN043200UV(response);
+                portType.qupcAR004030UVQUPCIN043200UV(buildQUPCIN043200UV01);
 
         if (ack == null) {
             throw new IllegalStateException("Tests fails, non null arknowledge was expected");
         }
+        
         System.out.println("Acknowledge : " + ack);
     }
 
+    /**
+     * JBoss specific way to customize the WSDL client (end point address).
+     * 
+     * @param portType the client to be customized.
+     */
     private static void setDefaultEndPointURI(QUPCAR004030UVPortType portType) {
         final BindingProvider bp = (BindingProvider) portType;
         final Map<String, Object> reqContext = bp.getRequestContext();
