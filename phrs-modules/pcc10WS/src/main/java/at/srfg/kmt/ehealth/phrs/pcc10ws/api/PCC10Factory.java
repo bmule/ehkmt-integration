0e9db3476b473827f3829721be3d69c0ed596290
mihai_radulescu <mopuffus@yahoo.com>
2011-04-18 10:14:37 +0200

    the interface is simple now

--- phrs-modules/pcc10WS/src/main/java/at/srfg/kmt/ehealth/phrs/pcc10ws/api/PCC10Factory.java
+++ phrs-modules/pcc10WS/src/main/java/at/srfg/kmt/ehealth/phrs/pcc10ws/api/PCC10Factory.java
@@ -9,23 +9,19 @@ package at.srfg.kmt.ehealth.phrs.pcc10ws.api;
 
 
 import java.util.Map;
+import net.sf.json.JSONObject;
 
 
 /**
  * Defines ways how a PCC 10(<code>QUPCIN043200UV01</code>) transaction 
- * request/response acknowledge are builded. The builded request/response
- * can have a default value or it can be based on a configuration.
- * The configuration is specified like a Map where the key and the values must 
- * have a certain (given type).
+ * request/response acknowledge are builded.
  * 
  * @param RESULT_TYPE the builded result type.
- * @param CONF_KEY_TYPE  the key type used in configuration.
- * @param CONF_VALUE_TYPE the value type used in configuration.
  * @version 0.1
  * @since 0.1
  * @author Mihai
  */
-public interface  PCC10Factory <RESULT_TYPE, CONF_KEY_TYPE, CONF_VALUE_TYPE> {
+public interface  PCC10Factory <RESULT_TYPE> {
 
     /**
      * Builds a certain request/response, the implementation decide if the 
@@ -36,13 +32,4 @@ public interface  PCC10Factory <RESULT_TYPE, CONF_KEY_TYPE, CONF_VALUE_TYPE> {
      */
     RESULT_TYPE build() throws PCC10BuildException;
     
-    /**
-     * Builds a request/response for a certain configuration, 
-     * the implementation decide if the result is a request or a response 
-     * acknowledge. 
-     * 
-     * @param config the configuration.
-     * @return a a request/response for a certain configuration.
-     */
-    RESULT_TYPE build(Map<CONF_KEY_TYPE, CONF_VALUE_TYPE> config) throws PCC10BuildException;
 }
