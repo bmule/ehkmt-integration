4f0276dd1d6a1c25be13615e1629125576ba2c67
mihai_radulescu <mopuffus@yahoo.com>
2011-04-19 08:16:54 +0200

    Flexible constants

--- phrs-modules/pcc10WS/src/main/java/at/srfg/kmt/ehealth/phrs/pcc10ws/impl/Constants.java
+++ phrs-modules/pcc10WS/src/main/java/at/srfg/kmt/ehealth/phrs/pcc10ws/impl/Constants.java
@@ -7,6 +7,11 @@
  */
 package at.srfg.kmt.ehealth.phrs.pcc10ws.impl;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
+
 
 /**
  *
@@ -15,11 +20,27 @@ package at.srfg.kmt.ehealth.phrs.pcc10ws.impl;
  * @author Mihai
  */
 public class Constants {
-
-    /**
-     * This the URL for the web service end point able to serve PCC10 queries.
+        /**
+     * This the URL for the web service end point able to serve PCC09 requests.
      * 
      */
-    final static String DEFAULT_LOCAL_PCC9_ENDPOINT_URL =
-            "http://127.0.0.1:8080/pcc10ws/QUPCAR004030UV_Service";
+    static final String DEFAULT_PCC_10_END_POINT;
+
+    static {
+        final ClassLoader classLoader = Constants.class.getClassLoader();
+        final InputStream stream = 
+                classLoader.getResourceAsStream("constants.properites");
+        final Properties properties = new Properties();
+        if (stream != null) {
+            try {
+                properties.load(stream);
+            } catch (IOException iOException) {
+                // I don't care. I use the default
+            }
+        }
+        final String key = "pcc10EndPoint";
+        DEFAULT_PCC_10_END_POINT = properties.contains(key) 
+                ? properties.getProperty(key).toString() 
+                : "http://127.0.0.1:8080/pcc10ws/QUPCAR004030UV_Service";
+    }
 }
