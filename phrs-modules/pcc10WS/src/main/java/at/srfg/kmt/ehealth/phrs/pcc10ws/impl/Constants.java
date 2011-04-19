cc08e63fc531ccb036eea7e2c5713a0fdbf14dca
mihai_radulescu <mopuffus@yahoo.com>
2011-04-19 13:05:16 +0200

    the default values for constants are really working.

--- phrs-modules/pcc10WS/src/main/java/at/srfg/kmt/ehealth/phrs/pcc10ws/impl/Constants.java
+++ phrs-modules/pcc10WS/src/main/java/at/srfg/kmt/ehealth/phrs/pcc10ws/impl/Constants.java
@@ -7,28 +7,38 @@
  */
 package at.srfg.kmt.ehealth.phrs.pcc10ws.impl;
 
+
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
 
 
-
 /**
+ * Contains all the constants used in the PCC10 WS module.
+ * The value for some contains can be configurated via a properties file named
+ * 'configuration.properties' placed in the classpath.
  *
  * @version 0.1
  * @since 0.1
  * @author Mihai
  */
 public class Constants {
-        /**
-     * This the URL for the web service end point able to serve PCC09 requests.
+
+    /**
+     * This the URL for the web service end point able to serve PCC10 requests.
+     * Its value can be configurated with by changing the value associated to
+     * the key <b>pcc10EndPoint</b>. Its default value is :
+     * <code>http://127.0.0.1:8080/pcc10ws/QUPCAR004030UV_Service</code>.
      * 
      */
     static final String DEFAULT_PCC_10_END_POINT;
 
+    /**
+     * Used to load the default values for the constants - if this is required.
+     */
     static {
         final ClassLoader classLoader = Constants.class.getClassLoader();
-        final InputStream stream = 
+        final InputStream stream =
                 classLoader.getResourceAsStream("constants.properties");
         final Properties properties = new Properties();
         if (stream != null) {
@@ -39,8 +49,8 @@ public class Constants {
             }
         }
         final String key = "pcc10EndPoint";
-        DEFAULT_PCC_10_END_POINT = properties.contains(key) 
-                ? properties.getProperty(key).toString() 
-                : "http://127.0.0.1:8080/pcc10ws/QUPCAR004030UV_Service";
+        DEFAULT_PCC_10_END_POINT = 
+                properties.getProperty(key, 
+                "http://127.0.0.1:8080/pcc10ws/QUPCAR004030UV_Service").toString().trim();;
     }
 }
