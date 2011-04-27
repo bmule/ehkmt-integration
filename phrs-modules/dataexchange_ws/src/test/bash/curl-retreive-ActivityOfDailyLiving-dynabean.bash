fd07b4ea51bd25586d87651e6042ad78d485bc3a
mihai_radulescu <mopuffus@yahoo.com>
2011-04-27 15:19:16 +0200

    the curl-retreive-ActivityOfDailiyLiving.bash is now out !

--- phrs-modules/dataexchange_ws/src/test/bash/curl-retreive-ActivityOfDailyLiving-dynabean.bash
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#set -xv
-
-HOST=http://localhost:8080
-CONTEXT=dataexchange_ws/dynamic_bean_repository
-
-# this is the here I retreive the last version for a a bean with class ...BodyWeight
-curl --fail  -X GET $HOST/$CONTEXT/getLastForClass?class_uri=at.srfg.kmt.ehealth.phrs.datamodel.impl.BodyWeight
- 
