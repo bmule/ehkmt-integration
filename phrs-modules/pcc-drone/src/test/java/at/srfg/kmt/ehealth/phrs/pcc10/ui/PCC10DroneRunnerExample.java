8a3bf416dc4db19df39a8ecbafad416c398e34ef
mihai_radulescu <mopuffus@yahoo.com>
2011-04-29 12:51:42 +0200

    it starts both drones

--- phrs-modules/pcc-drone/src/test/java/at/srfg/kmt/ehealth/phrs/pcc10/ui/PCC10DroneRunnerExample.java
+++ phrs-modules/pcc-drone/src/test/java/at/srfg/kmt/ehealth/phrs/pcc10/ui/PCC10DroneRunnerExample.java
@@ -9,7 +9,7 @@ package at.srfg.kmt.ehealth.phrs.pcc10.ui;
 
 
 /**
- * Shows how to start the PCC10 (Swing based drone). </br>
+ * Shows how to start the PCC10 and PCC09 (Swing based drones). </br>
  * To use this class from maven environment use :
  * mvn exec:java -Dexec.mainClass=at.srfg.kmt.ehealth.phrs.pcc10.ui.PCC10DroneRunnerExample -Dexec.classpathScope=test</br>
  * Take care the doesn not compile the classes.
@@ -26,6 +26,7 @@ public class PCC10DroneRunnerExample {
      * @param args the command line arguments. No arguments are required.
      */
     public static void main(String ... args) {
+        PCC09DroneRunner.start();
         PCC10DroneRunner.start();
     }
 }
