c927da944b4fdb22dd022455c69b87f62a19ff73
mihai_radulescu <mopuffus@yahoo.com>
2011-06-27 17:00:18 +0200

    all the constants are now static final

--- phrs-modules/phrs-constants/src/main/java/at/srfg/kmt/ehealth/phrs/Constants.java
+++ phrs-modules/phrs-constants/src/main/java/at/srfg/kmt/ehealth/phrs/Constants.java
@@ -24,162 +24,162 @@ import java.util.Properties;
  */
 public class Constants {
 
-    public static String COMMENT = "comment";
+    public static final String COMMENT = "comment";
 
-    public static String ICARDEA_NS = "http://www.icardea.at/phrs";
+    public static final String ICARDEA_NS = "http://www.icardea.at/phrs";
 
-    public static String STATUS_NS = getICardeaResource("status");
+    public static final String STATUS_NS = getICardeaResource("status");
 
-    public static String STATUS_STATE = STATUS_NS + "#statusState";
+    public static final String STATUS_STATE = STATUS_NS + "#statusState";
 
     /**
      * This is an instance URI.
      */
-    public static String STATUS_COMPELETE =
+    public static final String STATUS_COMPELETE =
             getICardeaResource("status/instance/Complete");
 
     /**
      * This is an instance URI.
      */
-    public static String STATUS_INCOMPELETE =
+    public static final String STATUS_INCOMPELETE =
             getICardeaResource("status/instance/Incomplete");
 
     /**
      * This is an instance URI.
      */
-    public static String STATUS_RUNNING =
+    public static final String STATUS_RUNNING =
             getICardeaResource("status/instance/Runnig");
 
     /**
      * This is an instance URI.
      */
-    public static String STATUS_INTERRUPTED =
+    public static final String STATUS_INTERRUPTED =
             getICardeaResource("status/instance/Interrupted");
 
-    public static String CODE_NS = getICardeaResource("code");
+    public static final String CODE_NS = getICardeaResource("code");
 
-    public static String CODE = CODE_NS + "#code";
+    public static final String CODE = CODE_NS + "#code";
 
-    public static String CODE_NAME = CODE_NS + "#codeName";
+    public static final String CODE_NAME = CODE_NS + "#codeName";
 
-    public static String CODE_SYSTEM = CODE_NS + "#codeSystem";
+    public static final String CODE_SYSTEM = CODE_NS + "#codeSystem";
 
-    public static String CODE_SYSTEM_NS = getICardeaResource("codeSystem");
+    public static final String CODE_SYSTEM_NS = getICardeaResource("codeSystem");
 
-    public static String CODE_SYSTEM_CODE = CODE_SYSTEM_NS + "#codeSystemCode";
+    public static final String CODE_SYSTEM_CODE = CODE_SYSTEM_NS + "#codeSystemCode";
 
-    public static String CODE_SYSTEM_NAME = CODE_SYSTEM_NS + "#codeSystemName";
+    public static final String CODE_SYSTEM_NAME = CODE_SYSTEM_NS + "#codeSystemName";
 
     /**
      * This is an instance URI.
      */
-    public static String LOINC_CODE_SYSTEM =
+    public static final String LOINC_CODE_SYSTEM =
             getICardeaResource("codeSystem/instance/LOINC");
 
     /**
      * This is an instance URI.
      */
-    public static String SNOMED_CODE_SYSTEM =
+    public static final String SNOMED_CODE_SYSTEM =
             getICardeaResource("codeSystem/instance/SNOMED");
 
     /**
      * This is an instance URI.
      */
-    public static String PHRS_CODE_SYSTEM =
+    public static final String PHRS_CODE_SYSTEM =
             getICardeaResource("codeSystem/instance/PHRS");
 
-    public static String BLOOD_PRESSURE_NS = getICardeaResource("bloodpressure");
+    public static final String BLOOD_PRESSURE_NS = getICardeaResource("bloodpressure");
 
-    public static String BLOOD_PRESSURE_DIASTOLIC = BLOOD_PRESSURE_NS + "#bpDiastolic";
+    public static final String BLOOD_PRESSURE_DIASTOLIC = BLOOD_PRESSURE_NS + "#bpDiastolic";
 
-    public static String HEART_NS = getICardeaResource("heart");
+    public static final String HEART_NS = getICardeaResource("heart");
 
-    public static String HEART_RATE = HEART_NS + "#bpHeartRate";
+    public static final String HEART_RATE = HEART_NS + "#bpHeartRate";
 
-    public static String OBSERVATION_DATE = "observationDate";
+    public static final String OBSERVATION_DATE = "observationDate";
 
-    public static String BODY_NS = getICardeaResource("body");
+    public static final String BODY_NS = getICardeaResource("body");
 
-    public static String BODY_BMI = BODY_NS + "#bodyBMI";
+    public static final String BODY_BMI = BODY_NS + "#bodyBMI";
 
-    public static String BODY_WEIGHT = BODY_NS + "#bodyweight";
+    public static final String BODY_WEIGHT = BODY_NS + "#bodyweight";
 
-    public static String HEIGHT = BODY_NS + "#height";
+    public static final String HEIGHT = BODY_NS + "#height";
 
-    public static String MEASURE_SYSTEM_NS = getICardeaResource("mSystem");
+    public static final String MEASURE_SYSTEM_NS = getICardeaResource("mSystem");
 
-    public static String MEASURE_SYSTEM_UNIT = MEASURE_SYSTEM_NS + "#mSystemValue";
+    public static final String MEASURE_SYSTEM_UNIT = MEASURE_SYSTEM_NS + "#mSystemValue";
 
-    public static String MEASURE_SYSTEM_VALUE = MEASURE_SYSTEM_NS + "#mSystemUnit";
+    public static final String MEASURE_SYSTEM_VALUE = MEASURE_SYSTEM_NS + "#mSystemUnit";
 
-    public static String MEASURE_SYSTEM_QUANTITY = MEASURE_SYSTEM_NS + "#mSystemQuantity";
+    public static final String MEASURE_SYSTEM_QUANTITY = MEASURE_SYSTEM_NS + "#mSystemQuantity";
     
 
-    public static String ICARDEA_INSTANCES_NS = ICARDEA_NS + "/instances";
+    public static final String ICARDEA_INSTANCES_NS = ICARDEA_NS + "/instances";
     
-    public static String ICARDEA_INSTANCES_MESURE_SYSTEM_NS = 
+    public static final String ICARDEA_INSTANCES_MESURE_SYSTEM_NS = 
             ICARDEA_INSTANCES_NS + "/MeasureSystem";
 
     /**
      * This is an instance URI.
      */
-    public static String MM_HG = ICARDEA_INSTANCES_MESURE_SYSTEM_NS + "#MmHg";
+    public static final String MM_HG = ICARDEA_INSTANCES_MESURE_SYSTEM_NS + "#MmHg";
 
     /**
      * This is an instance URI.
      */
-    public static String MILLIGRAM = ICARDEA_INSTANCES_MESURE_SYSTEM_NS + "#Milligram";
+    public static final String MILLIGRAM = ICARDEA_INSTANCES_MESURE_SYSTEM_NS + "#Milligram";
 
     /**
      * This is an instance URI.
      */
-    public static String TABLET = ICARDEA_INSTANCES_MESURE_SYSTEM_NS + "#Tablet";
+    public static final String TABLET = ICARDEA_INSTANCES_MESURE_SYSTEM_NS + "#Tablet";
 
     /**
      * This is an instance URI.
      */
-    public static String PILL = ICARDEA_INSTANCES_MESURE_SYSTEM_NS + "#Pill";
+    public static final String PILL = ICARDEA_INSTANCES_MESURE_SYSTEM_NS + "#Pill";
 
     /**
      * This is an instance URI.
      */
-    public static String CENTIMETER = ICARDEA_INSTANCES_MESURE_SYSTEM_NS + "#Centimeter";
+    public static final String CENTIMETER = ICARDEA_INSTANCES_MESURE_SYSTEM_NS + "#Centimeter";
 
     /**
      * This is an instance URI.
      */
-    public static String METER = ICARDEA_INSTANCES_MESURE_SYSTEM_NS + "#Meter";
+    public static final String METER = ICARDEA_INSTANCES_MESURE_SYSTEM_NS + "#Meter";
 
     /**
      * This is an instance URI.
      */
-    public static String KILOGRAM = ICARDEA_INSTANCES_MESURE_SYSTEM_NS + "#Kilogram";
+    public static final String KILOGRAM = ICARDEA_INSTANCES_MESURE_SYSTEM_NS + "#Kilogram";
 
     /**
      * This is an instance URI.
      */
-    public static String GRAM = ICARDEA_INSTANCES_MESURE_SYSTEM_NS + "#Gram";
+    public static final String GRAM = ICARDEA_INSTANCES_MESURE_SYSTEM_NS + "#Gram";
     
     
-    public static String ICARDEA_HL7V3_NS = ICARDEA_NS + "/hl7V3";
+    public static final String ICARDEA_HL7V3_NS = ICARDEA_NS + "/hl7V3";
 
-    public static String ICARDEA_HL7V3_TEMPLATE_ID_ROOT = ICARDEA_HL7V3_NS + "#templIdRoot";
+    public static final String ICARDEA_HL7V3_TEMPLATE_ID_ROOT = ICARDEA_HL7V3_NS + "#templIdRoot";
 
-    public static String ICARDEA_HL7V3_CODE = ICARDEA_HL7V3_NS + "#code";
+    public static final String ICARDEA_HL7V3_CODE = ICARDEA_HL7V3_NS + "#code";
     
-    public static String ICARDEA_HL7V3_EFFECTIVE_TIME = ICARDEA_HL7V3_NS + "#effectiveTime";
-    public static String ICARDEA_HL7V3_VALUE = ICARDEA_HL7V3_NS + "#value";
-    public static String ICARDEA_HL7V3_UNIT = ICARDEA_HL7V3_NS + "#unit";
+    public static final String ICARDEA_HL7V3_EFFECTIVE_TIME = ICARDEA_HL7V3_NS + "#effectiveTime";
+    public static final String ICARDEA_HL7V3_VALUE = ICARDEA_HL7V3_NS + "#value";
+    public static final String ICARDEA_HL7V3_UNIT = ICARDEA_HL7V3_NS + "#unit";
 
-    public static String ICARDEA_INSTANCE_SYSTOLIC_BLOOD_PREASURE =
+    public static final String ICARDEA_INSTANCE_SYSTOLIC_BLOOD_PREASURE =
             ICARDEA_NS + "/instances/SystolicBloodPressure";
 
-    public static String ICARDEA_INSTANCE_DIASTOLIC_BLOOD_PREASURE =
+    public static final String ICARDEA_INSTANCE_DIASTOLIC_BLOOD_PREASURE =
             ICARDEA_NS + "/instances/DiastolicBloodPressure";
 
     
-    public static String SKOS_NS = "http://www.w3.org/2004/02/skos/core";
-    public static String SKOS_NOTE = SKOS_NS + "#note";
+    public static final String SKOS_NS = "http://www.w3.org/2004/02/skos/core";
+    public static final String SKOS_NOTE = SKOS_NS + "#note";
 
     /**
      * Used to load the default values for the constants - if this is required.
