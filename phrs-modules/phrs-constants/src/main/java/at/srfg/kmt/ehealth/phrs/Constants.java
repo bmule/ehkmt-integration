c2d3664bcafa291fba3bcb3c0518ac9b0d16ae8f
mihai_radulescu <mopuffus@yahoo.com>
2011-08-17 16:08:39 +0200

    I add most of the constants for the vital signs

--- phrs-modules/phrs-constants/src/main/java/at/srfg/kmt/ehealth/phrs/Constants.java
+++ phrs-modules/phrs-constants/src/main/java/at/srfg/kmt/ehealth/phrs/Constants.java
@@ -86,33 +86,6 @@ public class Constants {
     public static final String PHRS_CODE_SYSTEM =
             getICardeaResource("codeSystem/instance/PHRS");
 
-    public static final String BLOOD_PRESSURE_NS = getICardeaResource("bloodpressure");
-
-    public static final String BLOOD_PRESSURE_DIASTOLIC = BLOOD_PRESSURE_NS + "#bpDiastolic";
-
-    public static final String HEART_NS = getICardeaResource("heart");
-
-    public static final String HEART_RATE = HEART_NS + "#bpHeartRate";
-
-    public static final String OBSERVATION_DATE = "observationDate";
-
-    public static final String BODY_NS = getICardeaResource("body");
-
-    public static final String BODY_BMI = BODY_NS + "#bodyBMI";
-
-    public static final String BODY_WEIGHT = BODY_NS + "#bodyweight";
-
-    public static final String HEIGHT = BODY_NS + "#height";
-
-    public static final String MEASURE_SYSTEM_NS = getICardeaResource("mSystem");
-
-    public static final String MEASURE_SYSTEM_UNIT = MEASURE_SYSTEM_NS + "#mSystemValue";
-
-    public static final String MEASURE_SYSTEM_VALUE = MEASURE_SYSTEM_NS + "#mSystemUnit";
-
-    public static final String MEASURE_SYSTEM_QUANTITY = MEASURE_SYSTEM_NS + "#mSystemQuantity";
-    
-
     public static final String ICARDEA_INSTANCES_NS = ICARDEA_NS + "/instances";
     
     public static final String ICARDEA_INSTANCES_MESURE_SYSTEM_NS = 
@@ -169,15 +142,140 @@ public class Constants {
     public static final String ICARDEA_HL7V3_VALUE = ICARDEA_HL7V3_NS + "#value";
     public static final String ICARDEA_HL7V3_UNIT = ICARDEA_HL7V3_NS + "#unit";
 
+    /**
+     * The URI for the instance used to define Systolic Blood Pressure in 
+     * IHE acceptation.<br/>
+     * This is a Vital Sign.
+     * 
+     */
+    public static final String ICARDEA_INSTANCE_BLOOD_PRESSURE =
+            ICARDEA_NS + "/instances/BloodPressure";
+
+    /**
+     * The URI for the instance used to define Systolic Blood Pressure in 
+     * IHE acceptation.<br/>
+     * This is a Vital Sign.
+     */
     public static final String ICARDEA_INSTANCE_SYSTOLIC_BLOOD_PRESSURE =
             ICARDEA_NS + "/instances/SystolicBloodPressure";
 
+    /**
+     * The URI for the instance used to define Diastolic Blood Pressure in 
+     * IHE acceptation.<br/>
+     * This is a Vital Sign.
+     */
     public static final String ICARDEA_INSTANCE_DIASTOLIC_BLOOD_PRERSSURE =
             ICARDEA_NS + "/instances/DiastolicBloodPressure";
 
+    /**
+     * The URI for the instance used to define Heart Rate in 
+     * IHE acceptation.<br/>
+     * This is a Vital Sign.
+     */
+    public static final String ICARDEA_INSTANCE_HEART_RATE =
+            ICARDEA_NS + "/instances/HeartRate";
+
+    /**
+     * The URI for the instance used to define Body Weight in 
+     * IHE acceptation.<br/>
+     * This is a Vital Sign.
+     */
+    public static final String ICARDEA_INSTANCE_ = 
+            ICARDEA_NS + "/instances/BodyWeight";
+
+    /**
+     * The URI for the instance used to define Body Weight in 
+     * IHE acceptation.<br/>
+     * This is a Vital Sign.
+     */
+    public static final String ICARDEA_INSTANCE_BODY_MASS_INDEX = 
+            ICARDEA_NS + "/instances/BodyMassIndex";
     
+    /**
+     * The URI for the instance used to define Body Height Measure in 
+     * IHE acceptation.<br/>
+     * This is a Vital Sign.
+     */
+    public static final String ICARDEA_INSTANCE_BODY_HEIGHT = 
+            ICARDEA_NS + "/instances/BodyHeightMeasure";
+    
+    public static final String ICARDEA_STATUS_ACTIVE = 
+            ICARDEA_NS + "/instances/Active";
+    
+    public static final String ICARDEA_STATUS_ABORTED = 
+            ICARDEA_NS + "/instances/Aborted";
+    
+    public static final String ICARDEA_STATUS_SUSPENDED = 
+            ICARDEA_NS + "/instances/Suspended";
+    
+    public static final String ICARDEA_MILLIMETER = 
+            ICARDEA_NS + "/instances/MeasureSystem#Millimeter";
+    
+    public static final String ICARDEA_CENTIMETER = 
+            ICARDEA_NS + "/instances/MeasureSystem#Centimeter";
+    
+    public static final String ICARDEA_MM_HG = 
+            ICARDEA_NS + "/instances/MeasureSystem#MmHg";
+    
+    public static final String ICARDEA_BPS = 
+            ICARDEA_NS + "/instances/MeasureSystem#Bps";
+    
+    public static final String ICARDEA_MILIGRAM = 
+            ICARDEA_NS + "/instances/MeasureSystem#Milligram";
+    
+    public static final String ICARDEA_METER = 
+            ICARDEA_NS + "/instances/MeasureSystem#Meter";
+    
+    public static final String ICARDEA_KILOGRAM = 
+            ICARDEA_NS + "/instances/MeasureSystem#Kilogram";
+    
+    public static final String ICARDEA_GRAM = 
+            ICARDEA_NS + "/instances/MeasureSystem#Gram";
+    
+    public static final String ICARDEA_TABLET = 
+            ICARDEA_NS + "/instances/MeasureSystem#Tablet";
+
     public static final String SKOS_NS = "http://www.w3.org/2004/02/skos/core";
     public static final String SKOS_NOTE = SKOS_NS + "#note";
+    
+    public static final String ITEM_ORDER = ICARDEA_NS + "#itemOrder";
+    
+    public static final String CREATE_DATE = ICARDEA_NS + "#createDate";
+    public static final String UPDATE_DATE = ICARDEA_NS + "#updateDate";
+    public static final String OWNER = ICARDEA_NS + "#owner";
+    public static final String CREATOR = ICARDEA_NS + "#creator";
+    
+    
+    /**
+     * The unique id for "Simple Observation" in the IHE acceptation.<br/>
+     * In most of the cases this is used like value (of type literal) for a 
+     * property named templIdRoot.
+     * 
+     * @see #ICARDEA_HL7V3_TEMPLATE_ID_ROOT
+     * @see <url>http://wiki.ihe.net/index.php?title=1.3.6.1.4.1.19376.1.5.3.1.4.13</url>
+     */
+    public static final String SIMPLE_OBSERVATIONS = "1.3.6.1.4.1.19376.1.5.3.1.4.13";
+    
+    /**
+     * The unique id for "Vital Signs Observation" in the IHE acceptation.<br/>
+     * In most of the cases this is used like value (of type literal) for a 
+     * property named templIdRoot.
+     * 
+     * @see #ICARDEA_HL7V3_TEMPLATE_ID_ROOT
+     * @see <url>http://wiki.ihe.net/index.php?title=1.3.6.1.4.1.19376.1.5.3.1.4.13.2</url>
+     */
+    public static final String VITAL_SIGNS_OBSERVATIONS = "1.3.6.1.4.1.19376.1.5.3.1.4.13.2";
+
+    /**
+     * The unique id for "Vital Signs Observation" in the IHE acceptation.<br/>
+     * In most of the cases this is used like value (of type literal) for a 
+     * property named templIdRoot.
+
+     * 
+     * @see #ICARDEA_HL7V3_TEMPLATE_ID_ROOT
+     * @see <url>http://wiki.ihe.net/index.php?title=1.3.6.1.4.1.19376.1.5.3.1.1.12.3.6</url>
+     */
+    public static final String ASTM_HL7CONTINUALITY_OF_CARE_DOCUMENT = "2.16.840.1.113883.10.20.1.31";
 
     /**
      * Used to load the default values for the constants - if this is required.
