42a0082a30664892582319f76cc433a46d13efad
mihai_radulescu <mopuffus@yahoo.com>
2011-08-18 15:27:23 +0200

    this example works

--- phrs-modules/phrs-dataexchange-client/src/test/java/at/srfg/kmt/ehealth/phrs/dataexchange/client/ProblemClientExample.java
+++ phrs-modules/phrs-dataexchange-client/src/test/java/at/srfg/kmt/ehealth/phrs/dataexchange/client/ProblemClientExample.java
@@ -11,32 +11,37 @@ import at.srfg.kmt.ehealth.phrs.persistence.api.TripleException;
 /**
  * Runnable class able to add and list on the standard output vital signs.
  * To use this class from maven environment use :
- * mvn exec:java -Dexec.mainClass=at.srfg.kmt.ehealth.phrs.dataexchange.client.VitalSignClientExample -Dexec.classpathScope=test</br>
+ * mvn exec:java -Dexec.mainClass=at.srfg.kmt.ehealth.phrs.dataexchange.client.ProblemClientExample -Dexec.classpathScope=test</br>
  * 
  * Take care the does not compile the classes.
  * @author mradules
  */
-public class VitalSignClientExample {
+public class ProblemClientExample {
 
     public static void main(String... args) throws GenericRepositoryException, TripleException {
         
-        final VitalSignClient client = new VitalSignClient();
+        final ProblemClient client = new ProblemClient();
         
-        client.addVitalSign(Constants.ICARDEA_INSTANCE_SYSTOLIC_BLOOD_PRESSURE,
-                "Free text note for systolic.",
+        // this adds a problem-symptom named fever
+        client. addProblem(Constants.HL7V3_SYMPTOM,
+                Constants.STATUS_COMPELETE,
                 "201006010000",
-                "100",
-                Constants.MM_HG);
-
-        client.addVitalSign(Constants.ICARDEA_INSTANCE_DIASTOLIC_BLOOD_PRERSSURE,
-                "Free text note for diasystolic.",
-                "201006010000",
-                "80",
-                Constants.MM_HG);
+                "201007010000",
+                "Free text note for the problem.",
+                Constants.HL7V3_FEVER);
         
-        final Iterable<Triple> vitalSigns = client.getVitalSigns();
-        for (Triple vitalSign : vitalSigns ) {
-            System.out.println(vitalSign);
+        // this adds a problem-symptom named temp loss of speech
+        client. addProblem(Constants.HL7V3_SYMPTOM,
+                Constants.STATUS_COMPELETE,
+                "201006010000",
+                "201007010000",
+                "Free text note for the problem.",
+                Constants.HL7V3_TEMPORRALY_LOSS_OF_SPEECH);
+        
+        final Iterable<Triple> problems = client.getProblems();
+        
+        for (Triple problem : problems ) {
+            System.out.println(problem);
         }
     }
 }
