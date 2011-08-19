5aa6c71106ef213559a88a0468345e1ec0b63260
mihai_radulescu <mopuffus@yahoo.com>
2011-08-19 20:48:53 +0200

    the very fris try

---
+++ phrs-modules/phrs-dataexchange-client/runVitalSignClient.bash
@@ -0,0 +1,11 @@
+#!/bin/bash
+#set -xv
+
+clear
+
+mvn clean 
+mvn compile
+mvn test-compile
+mvn exec:java -Dexec.mainClass=at.srfg.kmt.ehealth.phrs.dataexchange.client.VitalSignClientExample -Dexec.classpathScope=test
+
+echo "Done"
