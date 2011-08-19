81bfbb75c6219087f34cde5ad5aa5d7cce21cb95
mihai_radulescu <mopuffus@yahoo.com>
2011-08-19 17:16:12 +0200

    the very first try.

---
+++ phrs-modules/phrs-dataexchange-client/src/test/java/at/srfg/kmt/ehealth/phrs/dataexchange/client/TermClientUnitTest.java
@@ -0,0 +1,86 @@
+/*
+ * Project :iCardea
+ * File : TermClientUnitTest.java
+ * Encoding : UTF-8
+ * Date : Aug 19, 2011
+ * User : Mihai Radulescu
+ */
+package at.srfg.kmt.ehealth.phrs.dataexchange.client;
+
+
+import static org.junit.Assert.*;
+import at.srfg.kmt.ehealth.phrs.Constants;
+import at.srfg.kmt.ehealth.phrs.persistence.api.Triple;
+import at.srfg.kmt.ehealth.phrs.persistence.api.TripleException;
+import at.srfg.kmt.ehealth.phrs.persistence.impl.TriplestoreConnectionFactory;
+import org.junit.Before;
+import org.junit.After;
+import at.srfg.kmt.ehealth.phrs.persistence.api.GenericRepositoryException;
+import at.srfg.kmt.ehealth.phrs.persistence.api.GenericTriplestore;
+import at.srfg.kmt.ehealth.phrs.persistence.api.GenericTriplestoreLifecycle;
+import org.junit.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+/**
+ *
+ * @version 0.1
+ * @since 0.1
+ * @author mradules
+ */
+public class TermClientUnitTest {
+
+    /**
+     * The Logger instance. All log messages from this class
+     * are routed through this member. The Logger name space
+     * is <code>TermClientUnitTest</code>.
+     */
+    private static final Logger LOGGER =
+            LoggerFactory.getLogger(TermClientUnitTest.class);
+
+    private GenericTriplestore triplestore;
+    private TermClient termClient;
+
+    /**
+     * Runs before any test from this suite and prepare the environment for the
+     * next running test.
+     * 
+     * @throws GenericRepositoryException if this occurs then the test 
+     * environment may be wrong set.
+     */
+    @Before
+    public void initSuite() throws GenericRepositoryException {
+        final TriplestoreConnectionFactory connectionFactory =
+                TriplestoreConnectionFactory.getInstance();
+        triplestore = connectionFactory.getTriplestore();
+        termClient = new TermClient(triplestore);
+    }
+
+    /**
+     * Runs all after any test from this suite and cleans the environment in 
+     * order that the next test will get a 'clean' environment.
+     * 
+     * @throws GenericRepositoryException 
+     */
+    //@After
+    public void shutdownSuite() throws GenericRepositoryException {
+        ((GenericTriplestoreLifecycle) triplestore).shutdown();
+        ((GenericTriplestoreLifecycle) triplestore).cleanEnvironment();
+        triplestore = null;
+    }
+    
+    @Test
+    public void searchSymptomTerms() throws TripleException {
+        final Iterable<Triple> termsRelatedWith = 
+                termClient.getTermsRelatedWith(Constants.HL7V3_SYMPTOM);
+        assertTrue(termsRelatedWith.iterator().hasNext());
+    }
+    
+    @Test
+    public void searchConditionTerms() throws TripleException {
+        final Iterable<Triple> termsRelatedWith = 
+                termClient.getTermsRelatedWith(Constants.HL7V3_CONDITION);
+        assertFalse(termsRelatedWith.iterator().hasNext());
+    }
+}
