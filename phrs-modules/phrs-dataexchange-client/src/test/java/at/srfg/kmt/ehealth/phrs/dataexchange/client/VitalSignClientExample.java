f64522f52ed5dba971ac76a402f775e12ade4700
mihai_radulescu <mopuffus@yahoo.com>
2011-08-19 17:15:22 +0200

    it is working

--- phrs-modules/phrs-dataexchange-client/src/test/java/at/srfg/kmt/ehealth/phrs/dataexchange/client/VitalSignClientExample.java
+++ phrs-modules/phrs-dataexchange-client/src/test/java/at/srfg/kmt/ehealth/phrs/dataexchange/client/VitalSignClientExample.java
@@ -3,9 +3,10 @@ package at.srfg.kmt.ehealth.phrs.dataexchange.client;
 
 import at.srfg.kmt.ehealth.phrs.Constants;
 import at.srfg.kmt.ehealth.phrs.persistence.api.GenericRepositoryException;
+import at.srfg.kmt.ehealth.phrs.persistence.api.GenericTriplestore;
 import at.srfg.kmt.ehealth.phrs.persistence.api.Triple;
 import at.srfg.kmt.ehealth.phrs.persistence.api.TripleException;
-
+import at.srfg.kmt.ehealth.phrs.persistence.impl.TriplestoreConnectionFactory;
 
 
 /**
@@ -19,36 +20,52 @@ import at.srfg.kmt.ehealth.phrs.persistence.api.TripleException;
 public class VitalSignClientExample {
 
     public static void main(String... args) throws GenericRepositoryException, TripleException {
-        
-        final VitalSignClient client = new VitalSignClient();
-        
-        client.addVitalSign(Constants.ICARDEA_INSTANCE_SYSTOLIC_BLOOD_PRESSURE,
+
+        final String owner = "testOwner";
+        final TriplestoreConnectionFactory connectionFactory = 
+                TriplestoreConnectionFactory.getInstance();
+        final GenericTriplestore triplestore = connectionFactory.getTriplestore();
+
+        final VitalSignClient client = new VitalSignClient(triplestore);
+
+
+
+        client.addVitalSign(owner,
+                Constants.ICARDEA_INSTANCE_SYSTOLIC_BLOOD_PRESSURE,
                 "Free text note for systolic.",
                 "201006010000",
                 "100",
                 Constants.MM_HG);
 
-        client.addVitalSign(Constants.ICARDEA_INSTANCE_DIASTOLIC_BLOOD_PRERSSURE,
+        client.addVitalSign(owner,
+                Constants.ICARDEA_INSTANCE_DIASTOLIC_BLOOD_PRERSSURE,
                 "Free text note for diasystolic.",
                 "201006010000",
                 "80",
                 Constants.MM_HG);
-        
-        client.addVitalSign(Constants.ICARDEA_INSTANCE_BODY_HEIGHT,
+
+        client.addVitalSign(owner,
+                Constants.ICARDEA_INSTANCE_BODY_HEIGHT,
                 "Free text note for body height.",
                 "201006010000",
                 "180",
                 Constants.CENTIMETER);
-        
-        client.addVitalSign(Constants.ICARDEA_INSTANCE_BODY_WEIGHT,
+
+        client.addVitalSign(owner,
+                Constants.ICARDEA_INSTANCE_BODY_WEIGHT,
                 "Free text note for body weight.",
                 "201006010000",
                 "80",
                 Constants.KILOGRAM);
-        
-        final Iterable<Triple> vitalSigns = client.getVitalSigns();
-        for (Triple vitalSign : vitalSigns ) {
+
+        final Iterable<Triple> vitalSigns = client.getVitalSignsForUser(owner);
+        for (Triple vitalSign : vitalSigns) {
             System.out.println(vitalSign);
         }
+
+        // TAKE CARE !!!!!!
+        // This lines wipe out everithing alfter the client example ends its 
+        // main method.
+        client.cleanEnviroment();
     }
 }
