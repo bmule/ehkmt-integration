3095ba5fa6f211efda4a9df3d31703dec6f5346c
mradules <mradules@kis30.salzburgresearch.at>
2011-08-25 17:24:35 +0200

    add is working

--- phrs-modules/phrs-dataexchange-client/src/test/java/at/srfg/kmt/ehealth/phrs/dataexchange/client/VitalSignClientUnitTest.java
+++ phrs-modules/phrs-dataexchange-client/src/test/java/at/srfg/kmt/ehealth/phrs/dataexchange/client/VitalSignClientUnitTest.java
@@ -8,16 +8,23 @@
 package at.srfg.kmt.ehealth.phrs.dataexchange.client;
 
 
+import static at.srfg.kmt.ehealth.phrs.Constants.*;
+import static org.junit.Assert.*;
 import at.srfg.kmt.ehealth.phrs.persistence.api.GenericRepositoryException;
 import at.srfg.kmt.ehealth.phrs.persistence.api.GenericTriplestore;
 import at.srfg.kmt.ehealth.phrs.persistence.api.GenericTriplestoreLifecycle;
+import at.srfg.kmt.ehealth.phrs.persistence.api.Triple;
 import at.srfg.kmt.ehealth.phrs.persistence.api.TripleException;
+import at.srfg.kmt.ehealth.phrs.persistence.api.ValueType;
 import at.srfg.kmt.ehealth.phrs.persistence.impl.TriplestoreConnectionFactory;
+
+import java.util.HashSet;
+import java.util.Set;
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
 
 /**
  *
@@ -26,15 +33,15 @@ import org.slf4j.LoggerFactory;
  * @author mradules
  */
 public class VitalSignClientUnitTest {
-    
-        /**
+
+    /**
      * The Logger instance. All log messages from this class
      * are routed through this member. The Logger name space
      * is <code>TermClientUnitTest</code>.
      */
     private static final Logger LOGGER =
             LoggerFactory.getLogger(VitalSignClientUnitTest.class);
-
+    private static final String owner = VitalSignClientUnitTest.class.getName();
     private GenericTriplestore triplestore;
     private VitalSignClient vitalSignClient;
 
@@ -49,7 +56,7 @@ public class VitalSignClientUnitTest {
     public void initSuite() throws GenericRepositoryException, TripleException {
         final TriplestoreConnectionFactory connectionFactory =
                 TriplestoreConnectionFactory.getInstance();
-        triplestore = connectionFactory.getTriplestore();        
+        triplestore = connectionFactory.getTriplestore();
         vitalSignClient = new VitalSignClient(triplestore);
     }
 
@@ -66,5 +73,82 @@ public class VitalSignClientUnitTest {
         triplestore = null;
     }
 
-    
+    @Test
+    public void testAddVitalSignForOwner() throws TripleException {
+        final String resourceURI = 
+                vitalSignClient.addVitalSign(owner,
+                                            ICARDEA_INSTANCE_SYSTOLIC_BLOOD_PRESSURE,
+                                            "Free text note for systolic.",
+                                            "201006010000",
+                                            "100", MM_HG);
+        assertNotNull(resourceURI);
+
+        final Iterable<Triple> vitalSigns = vitalSignClient.getVitalSignsForUser(owner);
+        int count = 0;
+        Set<String> rootIds = new HashSet<String>();
+        for (Triple vitalSign : vitalSigns) {
+            final String predicate = vitalSign.getPredicate();
+            final String value = vitalSign.getValue();
+            if (predicate.equals(OWNER)) {
+                assertEquals(owner, value);
+            }
+            
+            if (predicate.equals(RDFS_TYPE)) {
+                assertEquals(PHRS_VITAL_SIGN_CLASS, value);
+            }
+            
+            if (predicate.equals(CREATE_DATE)) {
+                assertNotNull(value);
+            }
+            
+            if (predicate.equals(HL7V3_TEMPLATE_ID_ROOT)) {
+                rootIds.add(value);
+            }
+            
+            if (predicate.equals(HL7V3_CODE)) {
+                assertEquals(ICARDEA_INSTANCE_SYSTOLIC_BLOOD_PRESSURE, value);
+            }
+            
+            if (predicate.equals(SKOS_NOTE)) {
+                assertEquals("Free text note for systolic.", value);
+            }
+            
+            if (predicate.equals(EFFECTIVE_TIME)) {
+                assertEquals("201006010000", value);
+            }
+            
+            if (predicate.equals(HL7V3_VALUE)) {
+                assertEquals("100",value);
+            }
+            
+            if (predicate.equals(HL7V3_UNIT)) {
+                assertEquals(MM_HG,value);
+            }
+            
+            count++;
+        }
+        
+        final Set<String> expectedRootId = new HashSet<String>();
+        // all this three describes a vital sign
+        expectedRootId.add(SIMPLE_OBSERVATIONS);
+        expectedRootId.add(VITAL_SIGNS_OBSERVATIONS);
+        expectedRootId.add(ASTM_HL7CONTINUALITY_OF_CARE_DOCUMENT);
+        assertEquals(expectedRootId, rootIds);
+        
+        // the vial sign has 12 tripels, see the documentaion for VitalSignClient
+        assertEquals(12,count);
+        
+        final boolean exists = triplestore.exists(resourceURI);
+        assertTrue(exists);
+        final Iterable<String> forOwner = 
+                triplestore.getForPredicateAndValue(OWNER, owner, ValueType.LITERAL);
+        
+        count = 0;
+        for (String resource : forOwner) {
+            count++;
+        }
+        
+        // I expect only one vital sign for this owner
+        assertEquals(1,count);
+    }
 }
