1a76eb793e5012c1c92cce0912b566fa8d0c2a17
Mihai Radulescu <mradules@TZ-II-2-OG-Raum-09-Radulescu-MacOSX.local>
2011-08-25 09:23:16 +0200

    The very fristr try

---
+++ phrs-modules/phrs-dataexchange-client/src/test/java/at/srfg/kmt/ehealth/phrs/dataexchange/client/VitalSignClientUnitTest.java
@@ -0,0 +1,70 @@
+/*
+ * Project :iCardea
+ * File : VitalSignClientUnitTest.java
+ * Encoding : UTF-8
+ * Date : Aug 24, 2011
+ * User : Mihai Radulescu
+ */
+package at.srfg.kmt.ehealth.phrs.dataexchange.client;
+
+
+import at.srfg.kmt.ehealth.phrs.persistence.api.GenericRepositoryException;
+import at.srfg.kmt.ehealth.phrs.persistence.api.GenericTriplestore;
+import at.srfg.kmt.ehealth.phrs.persistence.api.GenericTriplestoreLifecycle;
+import at.srfg.kmt.ehealth.phrs.persistence.api.TripleException;
+import at.srfg.kmt.ehealth.phrs.persistence.impl.TriplestoreConnectionFactory;
+import org.junit.After;
+import org.junit.Before;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+/**
+ *
+ * @version 0.1
+ * @since 0.1
+ * @author mradules
+ */
+public class VitalSignClientUnitTest {
+    
+        /**
+     * The Logger instance. All log messages from this class
+     * are routed through this member. The Logger name space
+     * is <code>TermClientUnitTest</code>.
+     */
+    private static final Logger LOGGER =
+            LoggerFactory.getLogger(VitalSignClientUnitTest.class);
+
+    private GenericTriplestore triplestore;
+    private VitalSignClient vitalSignClient;
+
+    /**
+     * Runs before any test from this suite and prepare the environment for the
+     * next running test.
+     * 
+     * @throws GenericRepositoryException if this occurs then the test 
+     * environment may be wrong set.
+     */
+    @Before
+    public void initSuite() throws GenericRepositoryException, TripleException {
+        final TriplestoreConnectionFactory connectionFactory =
+                TriplestoreConnectionFactory.getInstance();
+        triplestore = connectionFactory.getTriplestore();        
+        vitalSignClient = new VitalSignClient(triplestore);
+    }
+
+    /**
+     * Runs all after any test from this suite and cleans the environment in 
+     * order that the next test will get a 'clean' environment.
+     * 
+     * @throws GenericRepositoryException 
+     */
+    @After
+    public void shutdownSuite() throws GenericRepositoryException {
+        ((GenericTriplestoreLifecycle) triplestore).shutdown();
+        ((GenericTriplestoreLifecycle) triplestore).cleanEnvironment();
+        triplestore = null;
+    }
+
+    
+}
