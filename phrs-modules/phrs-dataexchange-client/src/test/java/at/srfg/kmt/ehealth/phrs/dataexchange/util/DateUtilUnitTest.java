e72add3fafffc3f81bca0cfda21af4bbec3827fd
mradules <mradules@kis30.salzburgresearch.at>
2011-08-25 17:24:09 +0200

    I use a trim just to be sure

--- phrs-modules/phrs-dataexchange-client/src/test/java/at/srfg/kmt/ehealth/phrs/dataexchange/util/DateUtilUnitTest.java
+++ phrs-modules/phrs-dataexchange-client/src/test/java/at/srfg/kmt/ehealth/phrs/dataexchange/util/DateUtilUnitTest.java
@@ -25,7 +25,7 @@ public class DateUtilUnitTest {
     public void testToString_yyyyMMddHms() {
         final String formatedDate = DateUtil.getFormatedDate(new Date());
         assertNotNull(formatedDate);
-        final int length = formatedDate.length();
+        final int length = formatedDate.trim().length();
         // the lenght for the date is always 12
         assertEquals(12, length);
     }
