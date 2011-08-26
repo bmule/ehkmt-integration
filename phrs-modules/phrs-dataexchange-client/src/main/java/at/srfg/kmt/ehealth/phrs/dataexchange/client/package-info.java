608febdcef7f120a385423e28f02a2f2a26611fe
mihai <mihai@dyr.site>
2011-08-26 15:28:58 +0200

    the very first try

---
+++ phrs-modules/phrs-dataexchange-client/src/main/java/at/srfg/kmt/ehealth/phrs/dataexchange/client/package-info.java
@@ -0,0 +1,19 @@
+/*
+ * Project :iCardea
+ * File : package-info.java
+ * Encoding : UTF-8
+ * Date : Jun 27, 2011
+ * User : Mihai Radulescu
+ */
+package at.srfg.kmt.ehealth.phrs.dataexchange.client;
+
+
+/**
+ * Contains a lot of clients to the Generic Triplestore. 
+ * This clients are used to specialize the purpose of the Generic Triplestore 
+ * with bussiness specific details.
+ * 
+ * @version 0.1
+ * @since 0.1
+ * @author mradules
+ */
