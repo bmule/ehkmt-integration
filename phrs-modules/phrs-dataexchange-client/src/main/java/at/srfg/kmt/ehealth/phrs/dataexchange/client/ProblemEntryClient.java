3a4c4ff8948685f451bc32128e73d9d3e790716b
mradules <mradules@kis30.salzburgresearch.at>
2011-08-29 19:46:17 +0200

    a lot of new and working methods

--- phrs-modules/phrs-dataexchange-client/src/main/java/at/srfg/kmt/ehealth/phrs/dataexchange/client/ProblemEntryClient.java
+++ phrs-modules/phrs-dataexchange-client/src/main/java/at/srfg/kmt/ehealth/phrs/dataexchange/client/ProblemEntryClient.java
@@ -8,6 +8,7 @@
 package at.srfg.kmt.ehealth.phrs.dataexchange.client;
 
 
+import java.util.Map;
 import static at.srfg.kmt.ehealth.phrs.persistence.api.ValueType.*;
 import static at.srfg.kmt.ehealth.phrs.Constants.*;
 import at.srfg.kmt.ehealth.phrs.dataexchange.util.DateUtil;
@@ -22,18 +23,34 @@ import at.srfg.kmt.ehealth.phrs.persistence.api.Triple;
 import at.srfg.kmt.ehealth.phrs.persistence.api.TripleException;
 import at.srfg.kmt.ehealth.phrs.persistence.impl.sesame.LoadRdfPostConstruct;
 import at.srfg.kmt.ehealth.phrs.persistence.impl.sesame.SesameTriplestore;
+import java.util.HashMap;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+
 /**
- * Used to persist and retrieve vital signs information. <br/>
+ * Used to persist and retrieve Problem Entries information. <br/>
+ * A Problem entry can be used to describe following : 
+ * <ul>
+ * <li> Problem
+ * <li> Risk
+ * <li> Activities of daily living 
+ * <li> Symptom 
+ * <li>
+ * </ul>
+ * 
+ * To to mutate a <i>Problem Entry</i> in to one of he upper described types
+ * you need set the <code>estabilishCode</code> property with one of the proper 
+ * value, the value must be accorded with a controlled vocabulary (for more 
+ * information you need to use the constant class. </br>
+ * 
  * This class can not be extended. 
  * 
  * @version 0.1
  * @since 0.1
- * @author mradules
+ * @author Mihai
  */
-public class ProblemEntryClient {
+public final class ProblemEntryClient {
 
     /**
      * The Logger instance. All log messages from this class
@@ -43,8 +60,8 @@ public class ProblemEntryClient {
     private static final Logger LOGGER =
             LoggerFactory.getLogger(ProblemEntryClient.class);
     /**
-     * Holds the name for the creator, the instance responsible to create vital 
-     * signs instances with this client. 
+     * Holds the name for the creator, the instance responsible to create problem
+     * entry instances with this client. 
      */
     private static final String CREATOR_NAME = ProblemEntryClient.class.getName();
     /**
@@ -206,8 +223,17 @@ public class ProblemEntryClient {
      */
     public Iterable<Triple> getProblemEntriesForUser(String userId) throws TripleException {
 
+        final Map<String, String> queryMap = new HashMap<String, String>();
+        // like this I indetify the type
+        queryMap.put(RDFS_TYPE, PHRS_OBSERVATION_ENTRY_CLASS);
+        queryMap.put(OWNER, userId);
+
+        // here I search for all resources with 
+        // rdf type == prblem entry (observation entry) 
+        // and
+        // owner == user id
         final Iterable<String> resources =
-                triplestore.getForPredicateAndValue(OWNER, userId, LITERAL);
+                triplestore.getForPredicatesAndValues(queryMap);
 
         final MultiIterable result = new MultiIterable();
 
