c17bb084db7b633f71ee80f210066c9b917d6e84
mradules <mradules@kis30.salzburgresearch.at>
2011-08-30 22:03:58 +0200

    The PCC10 message can be wrote in to a file

--- phrs-modules/phrs-dataexchange-client/src/test/java/at/srfg/kmt/ehealth/phrs/dataexchange/client/VitalSignClientExample.java
+++ phrs-modules/phrs-dataexchange-client/src/test/java/at/srfg/kmt/ehealth/phrs/dataexchange/client/VitalSignClientExample.java
@@ -1,14 +1,20 @@
 package at.srfg.kmt.ehealth.phrs.dataexchange.client;
 
-
 import at.srfg.kmt.ehealth.phrs.Constants;
+import at.srfg.kmt.ehealth.phrs.dataexchange.ihe.VitalSignPCC10;
+import at.srfg.kmt.ehealth.phrs.dataexchange.util.QUPCAR004030UVUtil;
 import at.srfg.kmt.ehealth.phrs.persistence.api.GenericRepositoryException;
 import at.srfg.kmt.ehealth.phrs.persistence.api.GenericTriplestore;
 import at.srfg.kmt.ehealth.phrs.persistence.api.GenericTriplestoreLifecycle;
-import at.srfg.kmt.ehealth.phrs.persistence.api.Triple;
 import at.srfg.kmt.ehealth.phrs.persistence.api.TripleException;
 import at.srfg.kmt.ehealth.phrs.persistence.impl.TriplestoreConnectionFactory;
-
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+import javax.xml.bind.JAXBException;
+import org.apache.commons.beanutils.DynaBean;
+import org.hl7.v3.QUPCIN043200UV01;
 
 /**
  * Runnable class able to add and list on the standard output vital signs.
@@ -20,16 +26,16 @@ import at.srfg.kmt.ehealth.phrs.persistence.impl.TriplestoreConnectionFactory;
  * @author mradules
  */
 public class VitalSignClientExample {
-
-    public static void main(String... args) throws GenericRepositoryException, TripleException {
-
+    
+    public static void main(String... args) throws GenericRepositoryException, TripleException, IllegalAccessException, InstantiationException, JAXBException {
+        
         final String owner = "testOwner";
-        final TriplestoreConnectionFactory connectionFactory = 
+        final TriplestoreConnectionFactory connectionFactory =
                 TriplestoreConnectionFactory.getInstance();
         final GenericTriplestore triplestore = connectionFactory.getTriplestore();
-
+        
         final VitalSignClient client = new VitalSignClient(triplestore);
-
+        
         client.addVitalSign(owner,
                 Constants.ICARDEA_INSTANCE_SYSTOLIC_BLOOD_PRESSURE,
                 "Free text note for systolic.",
@@ -37,7 +43,7 @@ public class VitalSignClientExample {
                 Constants.STATUS_COMPELETE,
                 "100",
                 Constants.MM_HG);
-
+        
         client.addVitalSign(owner,
                 Constants.ICARDEA_INSTANCE_DIASTOLIC_BLOOD_PRERSSURE,
                 "Free text note for diasystolic.",
@@ -45,7 +51,7 @@ public class VitalSignClientExample {
                 Constants.STATUS_COMPELETE,
                 "80",
                 Constants.MM_HG);
-
+        
         client.addVitalSign(owner,
                 Constants.ICARDEA_INSTANCE_BODY_HEIGHT,
                 "Free text note for body height.",
@@ -53,7 +59,7 @@ public class VitalSignClientExample {
                 Constants.STATUS_COMPELETE,
                 "180",
                 Constants.CENTIMETER);
-
+        
         client.addVitalSign(owner,
                 Constants.ICARDEA_INSTANCE_BODY_WEIGHT,
                 "Free text note for body weight.",
@@ -61,11 +67,31 @@ public class VitalSignClientExample {
                 Constants.STATUS_COMPELETE,
                 "80",
                 Constants.KILOGRAM);
+        
+        
+        final Map<String, String> queryMap = new HashMap<String, String>();
+        // like this I indetify the type
+        queryMap.put(Constants.RDFS_TYPE,
+                Constants.PHRS_VITAL_SIGN_CLASS);
+        queryMap.put(Constants.OWNER, owner);
 
-        final Iterable<Triple> vitalSigns = client.getVitalSignsTriplesForUser(owner);
-        for (Triple vitalSign : vitalSigns) {
-            System.out.println(vitalSign);
+        // here I search for all resources with 
+        // rdf type == vital sign 
+        // and
+        // owner == user id
+        final Iterable<String> resources =
+                triplestore.getForPredicatesAndValues(queryMap);
+        
+        final DynaBeanClient dynaBeanClient = new DynaBeanClient(triplestore);
+        final Set<DynaBean> beans = new HashSet<DynaBean>();
+        for (String resoure : resources) {
+            final DynaBean dynaBean = dynaBeanClient.getDynaBean(resoure);
+            beans.add(dynaBean);
         }
+        
+        final QUPCIN043200UV01 pCC10Message = VitalSignPCC10.getPCC10Message(beans);
+        QUPCAR004030UVUtil.toWriteInTemp(pCC10Message, "vital-sign");
+
 
         // TAKE CARE !!!!!!
         // This lines wipe out everithing alfter the client example ends its 
