54b8047f8d8b94182af776bc0dfe31551bbf8e2d
mradules <mradules@kis30.salzburgresearch.at>
2011-08-30 22:02:59 +0200

    the very first try

---
+++ phrs-modules/phrs-dataexchange-client/src/main/java/at/srfg/kmt/ehealth/phrs/dataexchange/util/DynaBeanUtil.java
@@ -0,0 +1,58 @@
+/*
+ * Project :iCardea
+ * File : DynaBeanUtil.java
+ * Encoding : UTF-8
+ * Date : Aug 30, 2011
+ * User : Mihai Radulescu
+ */
+package at.srfg.kmt.ehealth.phrs.dataexchange.util;
+
+
+import org.apache.commons.beanutils.DynaBean;
+import org.apache.commons.beanutils.DynaClass;
+import org.apache.commons.beanutils.DynaProperty;
+
+
+/**
+ *
+ * @author Mihai
+ */
+public final class DynaBeanUtil {
+    
+    /**
+     * Don't let anybody to instantiate this class.
+     */
+    private DynaBeanUtil() {
+        // UNIMPLEMENTD
+    }
+
+    /**                                                                                                                                                                         
+     * Returns a String representation for a given <code>DynaBean</code>.                                                                                                       
+     *                                                                                                                                                                          
+     * @param bean the involved bean.                                                                                                                                           
+     * @return a String representation for a given <code>DynaBean</code>.                                                                                                       
+     */
+    public static String toString(DynaBean bean) {
+
+        if (bean == null) {
+            throw new NullPointerException("The bean argument can not be null.");
+        }
+
+        final StringBuffer msg = new StringBuffer();
+        final DynaClass dynaClass = bean.getDynaClass();
+        msg.append(dynaClass.getName());
+        msg.append(":{");
+        final DynaProperty[] properties = dynaClass.getDynaProperties();
+        for (DynaProperty property : properties) {
+            final String name = property.getName();
+            msg.append(name);
+            msg.append(":");
+            msg.append(bean.get(name));
+            msg.append(" ,");
+        }
+        // remove the last " ,"                                                                                                                                                 
+        msg.delete(msg.length() - 2, msg.length());
+
+        return msg.toString();
+    }
+}
