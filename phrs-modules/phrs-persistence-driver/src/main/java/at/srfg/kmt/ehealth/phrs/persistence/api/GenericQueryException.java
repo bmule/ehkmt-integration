2a24d919915a0556800056cf729492b0287cb576
Mihai Radulescu <mihai.radulescu@salzburgresearch.at>
2011-12-06 11:45:17 +0100

    better java doc

--- phrs-modules/phrs-persistence-driver/src/main/java/at/srfg/kmt/ehealth/phrs/persistence/api/GenericQueryException.java
+++ phrs-modules/phrs-persistence-driver/src/main/java/at/srfg/kmt/ehealth/phrs/persistence/api/GenericQueryException.java
@@ -7,8 +7,12 @@
  */
 package at.srfg.kmt.ehealth.phrs.persistence.api;
 
-
 /**
+ * Used to signal an error that occurs during a query opperation. The reason for
+ * this exception can be a triplestore specific exception. Most of the times it
+ * wraps the real cause for the exception, this cause can be obtained by using
+ * the
+ * <code>getCause()</code> method.
  *
  * @version 0.1
  * @since 0.1
@@ -16,19 +20,58 @@ package at.srfg.kmt.ehealth.phrs.persistence.api;
  */
 public class GenericQueryException extends Exception {
 
-    public GenericQueryException(Throwable thrwbl) {
-        super(thrwbl);
+    /**
+     * A version number for this class so that serialization can occur without
+     * worrying about the underlying class changing between serialization and
+     * deserialization.
+     */
+    private static final long serialVersionUID = 5165L;
+
+    /**
+     * Creates a new instance of
+     * <code>GenericQueryException</code> without detail message.
+     */
+    public GenericQueryException() {
+        // UNIMPLEMETNED
     }
 
-    public GenericQueryException(String string, Throwable thrwbl) {
-        super(string, thrwbl);
-    }
-
+    /**
+     * Constructs an instance of
+     * <code>GenericQueryException</code> with the specified detail message.
+     *
+     * @param msg the detail message.
+     */
     public GenericQueryException(String string) {
         super(string);
     }
 
-    public GenericQueryException() {
+    /**
+     * Constructs an
+     * <code>GenericQueryException</code> with a specified
+     * <code>Exception</code> cause, this cause can be obtained using the
+     * <code>getCause()</code> method.
+     *
+     * @param cause the cause for this exception. This is the exception to wrap
+     * and chain.
+     * @see Exception#getCause()
+     */
+    public GenericQueryException(Throwable thrwbl) {
+        super(thrwbl);
     }
-    
+
+    /**
+     * Constructs an
+     * <code>GenericRepositoryException</code> with a
+     * detail message and a specified <code>Exception</code>
+     * cause, this cause can be obtained using the
+     * <code>getCause()</code> method.
+     * 
+     * @param message the detail message.
+     * @param cause the cause for this exception. This is the
+     *            exception to wrap and chain.
+     * @see Exception#getCause()
+     */
+    public GenericQueryException(String string, Throwable thrwbl) {
+        super(string, thrwbl);
+    }
 }
