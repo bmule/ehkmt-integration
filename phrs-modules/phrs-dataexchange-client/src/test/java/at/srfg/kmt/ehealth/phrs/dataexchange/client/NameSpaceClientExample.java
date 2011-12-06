6f49253eada03c328d43d25bdade437e434c0094
Mihai Radulescu <mihai.radulescu@salzburgresearch.at>
2011-12-06 16:49:10 +0100

    it works, more comments

--- phrs-modules/phrs-dataexchange-client/src/test/java/at/srfg/kmt/ehealth/phrs/dataexchange/client/NameSpaceClientExample.java
+++ phrs-modules/phrs-dataexchange-client/src/test/java/at/srfg/kmt/ehealth/phrs/dataexchange/client/NameSpaceClientExample.java
@@ -5,13 +5,18 @@
  * Date     : Dec 6, 2011
  * User     : Mihai Radulescu
  */
+
+
 package at.srfg.kmt.ehealth.phrs.dataexchange.client;
 
+
 import at.srfg.kmt.ehealth.phrs.persistence.api.GenericTriplestore;
+import at.srfg.kmt.ehealth.phrs.persistence.api.TripleException;
 import at.srfg.kmt.ehealth.phrs.persistence.impl.TriplestoreConnectionFactory;
 
 /**
- *
+ * This runnable class register a relation between a name-space, a PHRS id and
+ * a protocol id, query for the protocol id and displays it.
  * To run this class from maven environment use :
  * <pre>
  * mvn exec:java -Dexec.mainClass=at.srfg.kmt.ehealth.phrs.dataexchange.client.NameSpaceClientExample -Dexec.classpathScope=test<br/>
@@ -21,19 +26,52 @@ import at.srfg.kmt.ehealth.phrs.persistence.impl.TriplestoreConnectionFactory;
  * @version 0.1
  * @since 0.1
  */
-public class NameSpaceClientExample {
+public final class NameSpaceClientExample {
+
+    /**
+     * The unique name space identifier used in this test.
+     */
+    private static final String NAME_SPACE = "MY NAME SPACE";
+
+    /**
+     * The unique PHR System identifier used in this test.
+     */
+    private static final String PHRS_ID = "MY PHRS_ID";
+
+    /**
+     * The unique Protocol Id identifier used in this test.
+     */
+    private static final String PROTOCOL_ID = "MY PROTOCOL_ID";
+    
+    /**
+     * Don't let anyone to instantiate this class.
+     */
+    private NameSpaceClientExample() {
+        // UNIMPLEMETNED
+    }
 
     /**
      * Run this class from the command line.
      *
      * @param args the command lines arguments - no arguments are required.
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws TripleException {
 
         final TriplestoreConnectionFactory connectionFactory =
                 TriplestoreConnectionFactory.getInstance();
         final GenericTriplestore triplestore = connectionFactory.getTriplestore();
         final NameSpaceClient nameSpaceClient = new NameSpaceClient(triplestore);
+        // I register the relation between the name space, phrs id and 
+        // protocol id.
+        nameSpaceClient.register(NAME_SPACE, PHRS_ID, PROTOCOL_ID);
 
+
+        // I register the relation between the name space, phrs id and 
+        // protocol id.
+        final String protolId =
+                nameSpaceClient.getProtocolId(NAME_SPACE, PHRS_ID);
+        final String msg =
+                String.format("The protocol id for name space : %s and phrs id : %s is %s", NAME_SPACE, PHRS_ID, protolId);
+        System.out.println(msg);
     }
 }
