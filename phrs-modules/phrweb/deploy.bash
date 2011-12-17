2375867b26fb81beb1240e24fab38126a52e5238
mihai <mihai@new-host.home>
2011-12-17 01:56:52 +0100

    more comments

--- phrs-modules/phrweb/deploy.bash
+++ phrs-modules/phrweb/deploy.bash
@@ -1,11 +1,17 @@
 #!/bin/bash
 #set -xv
 
+# this bash script is used to pack and deploy the phrweb project in to an
+# existing Tomcat instance. 
+
 clear
 
 ./package.bash
 
 tomcat_home="/lab0/apache/tomcat/apache-tomcat-6.0.35/"
 
-#mvn  cargo:undeploy  -Dcatalina.home=$tomcat_home
-mvn  cargo:deploy    -Dcatalina.home=$tomcat_home
+mvn  cargo:deploy  -Dcatalina.home=$tomcat_home
+
+echo ""
+echo "The phrweb is deployed on :"$tomcat_home
+echo ""
--- phrs-modules/phrweb/package.bash
+++ phrs-modules/phrweb/package.bash
@@ -1,17 +1,21 @@
 #!/bin/bash
 #set -xv
 
+# this bash script is used to pack all the phrweb project in to an
+# singular war file. The location of the file is target/ directory. 
+
 clear
 
 mvn clean
 
 mkdir   target
+
 tar  zxf src/main/assembly/classes.tgz
 mv classes  target/classes/
 
 mvn package -DskipTests=true
 
 
-echo "Package was done"
-echo
-echo
+echo ""
+echo "The phrs war file is ready."
+echo ""
