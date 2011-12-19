a51fe9d9a98711caea08f55bb0961248687cb88c
Mihai Radulescu <mradules@kis30.salzburgresearch.at>
2011-12-19 11:51:00 +0100

    The vocabulary_ws module was deleted

--- phrs-modules/vocabulary_ws/src/test/bash/curl-dynamic_bean_repository-test.bash
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-#set -xv
-
-HOST=http://localhost:8080
-CONTEXT=dataexchange_ws/dynamic_bean_repository
-
-curl --fail -X GET $HOST/$CONTEXT/test
--- phrs-modules/vocabulary_ws/src/test/bash/curl-pull-Risk.bash
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#set -xv
-
-HOST=http://localhost:8080
-CONTEXT=dataexchange_ws/dynamic_bean_repository
-
-# phrsBeanClassURI : at.srfg.kmt.ehealth.phrs.datamodel.impl.Risk
-curl --fail  -X GET $HOST/$CONTEXT/getLastForClass?class_uri=at.srfg.kmt.ehealth.phrs.datamodel.impl.Risk
- 
