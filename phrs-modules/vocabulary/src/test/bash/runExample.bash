512ae83ca3211d63c46922c2b933626d7058b2ea
Mihai Radulescu <mradules@kis30.salzburgresearch.at>
2011-12-19 11:50:22 +0100

    The vocabulary module was deleted

--- phrs-modules/vocabulary/src/test/bash/runExample.bash
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-# this script must run in the directory that contains the pom file.
-
-
-mvn compiler:testCompile -DskipTests=true;mvn exec:java -Dexec.mainClass="at.srfg.kmt.ehealth.phrs.dataexchange.impl.DymanicClassRepositoryBeanExample"  -Dexec.classpathScope=test
