7bde377780fe982905577edf1711725d59394146
mradules <mradules@kis30.salzburgresearch.at>
2011-12-20 22:14:13 +0100

    just a clean before compile

--- phrs-modules/phrs-soap-pcc09ws/startPCC9Endpoint.bash
+++ phrs-modules/phrs-soap-pcc09ws/startPCC9Endpoint.bash
@@ -3,7 +3,7 @@
 
 clear
 
-mvn compile
+mvn clean compile
 mvn test-compile
 mvn exec:java -Dexec.mainClass=at.srfg.kmt.ehealth.phrs.ws.soap.PCC9EndpointExample -Dexec.classpathScope=test -Dexec.args="localhost 8989 testws/pcc9"
 
