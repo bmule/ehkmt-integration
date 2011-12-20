e4e83415d6d96b1be8738e9eb304d0c01fe14190
mihai <mihai@new-host.home>
2011-12-20 16:26:13 +0100

    unit tests for update and delete

--- phrs-modules/phrs-dataexchange-client/src/test/java/at/srfg/kmt/ehealth/phrs/dataexchange/client/MedicationClientUnitTest.java
+++ phrs-modules/phrs-dataexchange-client/src/test/java/at/srfg/kmt/ehealth/phrs/dataexchange/client/MedicationClientUnitTest.java
@@ -7,6 +7,7 @@
  */
 package at.srfg.kmt.ehealth.phrs.dataexchange.client;
 
+
 import at.srfg.kmt.ehealth.phrs.persistence.api.Triple;
 import at.srfg.kmt.ehealth.phrs.persistence.api.ValueType;
 import java.util.HashSet;
@@ -18,6 +19,7 @@ import at.srfg.kmt.ehealth.phrs.persistence.api.GenericTriplestore;
 import at.srfg.kmt.ehealth.phrs.persistence.api.GenericTriplestoreLifecycle;
 import at.srfg.kmt.ehealth.phrs.persistence.api.TripleException;
 import at.srfg.kmt.ehealth.phrs.persistence.impl.TriplestoreConnectionFactory;
+import java.util.Iterator;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
@@ -25,21 +27,33 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 /**
+ * Used to prove the functionality for the
+ * <code>MedicationClient</code>.
  *
- * @author mradules
+ * @author Mihai
+ * @version 0.1
+ * @since 0.1
+ * @see MedicationClient
  */
 public class MedicationClientUnitTest {
 
-    public static final String NOTE = "Free text note for medication.";
-    private static final String USER = MedicationClientUnitTest.class.getName();
     /**
-     * The Logger instance. All log messages from this class
-     * are routed through this member. The Logger name space
-     * is <code>TermClientUnitTest</code>.
+     * The note used in this test
+     */
+    public static final String NOTE = "Free text note for medication.";
+
+    private static final String USER = MedicationClientUnitTest.class.getName();
+
+    /**
+     * The Logger instance. All log messages from this class are routed through
+     * this member. The Logger name space is
+     * <code>TermClientUnitTest</code>.
      */
     private static final Logger LOGGER =
             LoggerFactory.getLogger(VitalSignClientUnitTest.class);
+
     private GenericTriplestore triplestore;
+
     private MedicationClient medicationClient;
 
     public MedicationClientUnitTest() {
@@ -48,8 +62,8 @@ public class MedicationClientUnitTest {
     /**
      * Runs before any test from this suite and prepare the environment for the
      * next running test.
-     * 
-     * @throws GenericRepositoryException if this occurs then the test 
+     *
+     * @throws GenericRepositoryException if this occurs then the test
      * environment may be wrong set.
      */
     @Before
@@ -61,10 +75,10 @@ public class MedicationClientUnitTest {
     }
 
     /**
-     * Runs all after any test from this suite and cleans the environment in 
+     * Runs all after any test from this suite and cleans the environment in
      * order that the next test will get a 'clean' environment.
-     * 
-     * @throws GenericRepositoryException 
+     *
+     * @throws GenericRepositoryException
      */
     @After
     public void shutdownSuite() throws GenericRepositoryException {
@@ -73,6 +87,11 @@ public class MedicationClientUnitTest {
         triplestore = null;
     }
 
+    /**
+     * Adds a single medication and prove if this was done properly.
+     *
+     * @throws TripleException if this exception occurs then this test fails.
+     */
     @Test
     public void testAddMedicationForOwner() throws TripleException {
         final String resourceURI =
@@ -130,8 +149,8 @@ public class MedicationClientUnitTest {
 
         // the medication has 13 tripels, see the documentaion for VitalSignClient
         assertEquals(14, count);
-        
-                final boolean exists = triplestore.exists(resourceURI);
+
+        final boolean exists = triplestore.exists(resourceURI);
         assertTrue(exists);
         final Iterable<String> forOwner =
                 triplestore.getForPredicateAndValue(Constants.OWNER, USER, ValueType.LITERAL);
@@ -143,6 +162,117 @@ public class MedicationClientUnitTest {
 
         // I expect only one vital sign for this owner
         assertEquals(1, count);
+    }
+
+    /**
+     * Registers a medication and after this it updates the dosage for the
+     * registered medication.
+     *
+     * @throws TripleException if this exception occurs then this test fails.
+     */
+    @Test
+    public void testUpdateMedicationDosageOwner() throws TripleException {
+        final String resourceURI =
+                medicationClient.addMedicationSign(
+                USER,
+                NOTE,
+                Constants.STATUS_COMPELETE,
+                "201006010000",
+                "201006010000",
+                "MyFreqency",
+                Constants.HL7V3_ORAL_ADMINISTRATION,
+                "1",
+                "pillURI",
+                "MyDrug");
+        assertNotNull(resourceURI);
+
+        // create a new dosage
+        final String newDossageValue = "1.2";
+        final String newDosageUnit = Constants.PILL;
+        final String newDosageURI = medicationClient.buildDosage(newDossageValue, newDosageUnit);
+        assertNotNull(newDosageURI);
+
+        // update the existentn dosage
+        medicationClient.updateMedication(resourceURI,
+                Constants.PHRS_MEDICATION_DOSAGE,
+                newDosageURI);
+
+        final Iterable<String> medicationURIForUser =
+                medicationClient.getMedicationURIsForUser(USER);
+        final Iterator<String> iterator = medicationURIForUser.iterator();
+        // I only have one medication
+        assertTrue(iterator.hasNext());
+        final String medicationURI = iterator.next();
+        final Iterable<String> dosageURIs =
+                triplestore.getForSubjectAndPredicate(medicationURI, Constants.PHRS_MEDICATION_DOSAGE);
+        
+        proveDosage(dosageURIs.iterator().next(), newDossageValue, newDosageUnit);
+    }
+
+    /**
+     * Proves id the resource identified after its URI is a PHR Medication
+     * Dosage and it proves also if its arguments are the expected one.
+     *
+     * @param dossageURI the URI for the PHR Medication Dosage.
+     * @param value the expected value for the PHR Medication Dosage.
+     * @param unit the expected unit for the PHR Medication Dosage.
+     * @throws TripleException if this exception occurs then this test fails.
+     */
+    private void proveDosage(String dossageURI, String value, String unit)
+            throws TripleException {
+
+        if (dossageURI == null || value == null || unit == null) {
+            throw new NullPointerException("None of the proveDosage methods argumetns ca be null.");
+        }
+
+        final Iterable<Triple> dosageTriples = triplestore.getForSubject(dossageURI);
+        for (Triple triple : dosageTriples) {
+            final String predicate = triple.getPredicate();
+            final String val = triple.getValue();
+
+            if (Constants.RDFS_TYPE.equals(predicate)) {
+                val.equals(Constants.PHRS_MEDICATION_DOSAGE_CLASS);
+            }
+
+            if (Constants.HL7V3_DOSAGE_VALUE.equals(predicate)) {
+                assertEquals(value, val);
+            }
+
+            if (Constants.HL7V3_DOSAGE_UNIT.equals(predicate)) {
+                assertEquals(unit, val);
+            }
+        }
+    }
+
+    /**
+     * Deletes a medication and proves is the delete was done properly.
+     *
+     * @throws TripleException if this exception occurs then this test fails.
+     */
+    @Test
+    public void testDeleteMedication() throws TripleException {
+        final String resourceURI =
+                medicationClient.addMedicationSign(
+                USER,
+                NOTE,
+                Constants.STATUS_COMPELETE,
+                "201006010000",
+                "201006010000",
+                "MyFreqency",
+                Constants.HL7V3_ORAL_ADMINISTRATION,
+                "1",
+                "pillURI",
+                "MyDrug");
+        assertNotNull(resourceURI);
+
+        medicationClient.deleteMedication(resourceURI);
+        final Iterable<Triple> medicationTriples = medicationClient.getMedicationTriples();
+        int counter = 0;
+        for (Triple triple : medicationTriples) {
+            counter++;
+        }
+
+        assertEquals(0, counter);
 
     }
 }
