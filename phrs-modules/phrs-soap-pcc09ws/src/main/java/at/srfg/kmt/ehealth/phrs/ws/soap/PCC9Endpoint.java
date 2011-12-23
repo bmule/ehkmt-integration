09dc2ae87f84e5f313d7742a1c28e9b4f10978a6
mihai <mihai@new-host.home>
2011-12-23 17:25:15 +0100

    more java doc

--- phrs-modules/phrs-soap-pcc09ws/src/main/java/at/srfg/kmt/ehealth/phrs/ws/soap/PCC9Endpoint.java
+++ phrs-modules/phrs-soap-pcc09ws/src/main/java/at/srfg/kmt/ehealth/phrs/ws/soap/PCC9Endpoint.java
@@ -15,6 +15,10 @@ import org.slf4j.LoggerFactory;
 
 
 /**
+ * This class is en end point able to accept <a
+ * href="http://wiki.ihe.net/index.php?title=PCC-9">PCC09</a> transactions.<br/>
+ * The usage is simple : build an instance for a given host, port and context
+ * path and then use the <code>start</code> method.
  *
  * @author mihai
  * @version 1.0-SNAPSHOT
@@ -30,9 +34,36 @@ public class PCC9Endpoint {
     private static final Logger LOGGER =
             LoggerFactory.getLogger(PCC9Endpoint.class);
 
+    /**
+     * The URL for end point.
+     */
     private final String endpointURI;
 
+    /**
+     * Builds a end point for 
+     * <a href="http://wiki.ihe.net/index.php?title=PCC-9">PCC09</a>
+     * transactions for a given list of arguments.
+     *
+     * @param host the host i.p. for the end point, it can not be null.
+     * @param port the port for the end point,
+     * @param path path i.p. for the end point, it can not be null.
+     * @throws NullPointerException if the <code>host</code> or 
+     * the <code>path</code> is null.
+     */
     public PCC9Endpoint(String host, int port, String path) {
+
+        if (host == null) {
+            final NullPointerException exception =
+                    new NullPointerException("The host argument can not be null.");
+            LOGGER.error(exception.getMessage(), exception);
+        }
+
+        if (path == null) {
+            final NullPointerException exception =
+                    new NullPointerException("The path argument can not be null.");
+            LOGGER.error(exception.getMessage(), exception);
+        }
+
         final StringBuilder builder = new StringBuilder("http://");
         builder.append(host);
         builder.append(":");
@@ -42,11 +73,15 @@ public class PCC9Endpoint {
         endpointURI = builder.toString();
     }
 
+    /**
+     * Starts the end point.
+     *
+     * @throws MalformedURLException if the URL defined in the constructor is 
+     * malformated.
+     */
     public void start() throws MalformedURLException {
         LOGGER.info("PCC9 endpoint runs on {}", endpointURI);
         final QUPCAR004040UVWebService webService = new QUPCAR004040UVWebService();
-        
-        //final QUPCAR004040UVService service = getQUPCAR004040UVService();
         Endpoint.publish(endpointURI, webService);
     }
 }
