fd0e04920d6b4a69558b5775200cb957e204df05
mopuffus <mopuffus@yahoo.com>
2012-01-09 14:36:28 +0100

    more java docs

--- phrs-modules/phrs-soap-pcc09ws/src/test/java/at/srfg/kmt/ehealth/phrs/ws/soap/QueryFactoryUnitTest.java
+++ phrs-modules/phrs-soap-pcc09ws/src/test/java/at/srfg/kmt/ehealth/phrs/ws/soap/QueryFactoryUnitTest.java
@@ -12,39 +12,46 @@ import javax.xml.bind.JAXBException;
 import org.hl7.v3.CD;
 import org.hl7.v3.QUPCIN043100UV01;
 import org.hl7.v3.QUPCIN043100UV01QUQIMT020001UV01ControlActProcess;
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
 import org.junit.Test;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 /**
+ * Tests the functionality for the <code>QueryFactory</code> class. <br/>
+ * This class can not be extended.
  *
  * @author mihai
  * @version 1.0-SNAPSHOT
  * @since 1.0-SNAPSHOT
+ * @see QueryFactory
  */
-public class QueryFactoryUnitTest {
+public final class QueryFactoryUnitTest {
 
     /**
-     * The Logger instance. All log messages from this class are routed through
-     * this member. The Logger name space is
-     * <code>at.srfg.kmt.ehealth.phrs.ws.soap.QueryFactoryUnitTest</code>.
+     * Builds a <code>QueryFactoryUnitTest</code> instance.
      */
-    private static final Logger LOGGER =
-            LoggerFactory.getLogger(QueryFactoryUnitTest.class);
-
     public QueryFactoryUnitTest() {
         // UNIMPLEMENTED
     }
 
+    /**
+     * Builds an empty PCC9 request and proves if the Control Act Process Code
+     * (from the new builded PCC9 request) has the right value.
+     *
+     * @throws JAXBException by any JAXB related exception, if this exception
+     * occurs then this test fails.
+     */
     @Test
-    public void testBuildQUPCIN043100UV01() throws JAXBException {
-        final QUPCIN043100UV01 query = QueryFactory.buildQUPCIN043100UV01();
-        assertNotNull(query);
-        final QUPCIN043100UV01QUQIMT020001UV01ControlActProcess controlActProcess = 
+    public void testBuildEmptyPCC9Request() throws JAXBException {
+        final QUPCIN043100UV01 query = QueryFactory.buildEmptyPCC9Request();
+        assertNotNull("The PCC9 request can not be null.", query);
+        final QUPCIN043100UV01QUQIMT020001UV01ControlActProcess controlActProcess =
                 query.getControlActProcess();
         final CD code = controlActProcess.getCode();
         final String codeStr = code.getCode();
-        assertEquals("QUPC_TE043100UV", codeStr);
+        final String expectedCode = "QUPC_TE043100UV";
+        assertEquals("This PCC9 message must contains the code : " +  codeStr,
+                expectedCode, codeStr);
+        
     }
 }
