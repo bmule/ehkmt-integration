f18565581978dfa6d335626b0d5fb6da05839c5c
mopuffus <mopuffus@yahoo.com>
2012-01-10 14:43:20 +0100

    more java docs

--- phrs-modules/phrs-message-dispatcher/src/main/java/at/srfg/kmt/ehealth/phrs/dispatch/impl/PCC10TaskFactory.java
+++ phrs-modules/phrs-message-dispatcher/src/main/java/at/srfg/kmt/ehealth/phrs/dispatch/impl/PCC10TaskFactory.java
@@ -2,7 +2,7 @@
  * Project :iCardea
  * File : PCC10TaskFactory.java
  * Encoding : UTF-8
- * Date : Jan 10, 2012
+ * Date : Dec 21, 2011
  * User : Mihai Radulescu
  */
 package at.srfg.kmt.ehealth.phrs.dispatch.impl;
