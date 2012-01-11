315b1dd9f39e536217e70ea361a6bba8cd3fdd19
mopuffus <mopuffus@yahoo.com>
2012-01-11 15:46:25 +0100

    the very first try

---
+++ phrs-modules/phrs-soap-pcc10ws/send-pcc10-problem.bash
@@ -0,0 +1,9 @@
+#!/bin/bash
+#set -xv
+
+clear
+
+mvn compile
+mvn test-compile
+
+mvn exec:java -Dexec.mainClass=at.srfg.kmt.ehealth.phrs.ws.soap.pcc10.ProblemClientFeverSymptomExample -Dexec.classpathScope=test -Dexec.args="http://localhost:8989/testws/pcc10"
