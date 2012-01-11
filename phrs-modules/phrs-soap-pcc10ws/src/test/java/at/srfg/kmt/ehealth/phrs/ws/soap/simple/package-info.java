2853cc0b48b00151c8990ce03af5ae18768830ac
mopuffus <mopuffus@yahoo.com>
2012-01-11 11:30:01 +0100

    bbetter java doc

---
+++ phrs-modules/phrs-soap-pcc10ws/src/test/java/at/srfg/kmt/ehealth/phrs/ws/soap/simple/package-info.java
@@ -0,0 +1,18 @@
+/*
+ * Project :iCardea
+ * File : package-info.java
+ * Encoding : UTF-8
+ * Date : Dec 9, 2011
+ * User : Mihai Radulescu
+ */
+
+package at.srfg.kmt.ehealth.phrs.ws.soap.simple;
+
+
+/**
+ * Contains a simple SOAP Web service only with test purposes.
+ *
+ * @author Miahai
+ * @version 1.0-SNAPSHOT
+ * @since 1.0-SNAPSHOT
+ */
