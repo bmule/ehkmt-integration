13cee2001b5edb95bf3432b2b28b08e4d7dbbf0e
mopuffus <mopuffus@yahoo.com>
2012-01-11 15:09:44 +0100

    I call the right class (from the right package)

--- phrs-modules/phrs-soap-pcc10ws/start-pcc10-endpoint.bash
+++ phrs-modules/phrs-soap-pcc10ws/start-pcc10-endpoint.bash
@@ -5,6 +5,6 @@ clear
 
 mvn clean compile
 mvn test-compile
-mvn exec:java -Dexec.mainClass=at.srfg.kmt.ehealth.phrs.ws.soap.PCC10EndpointExample -Dexec.classpathScope=test -Dexec.args="localhost 8989 testws/pcc10"
+mvn exec:java -Dexec.mainClass=at.srfg.kmt.ehealth.phrs.ws.soap.pcc10.PCC10EndpointExample -Dexec.classpathScope=test -Dexec.args="localhost 8989 testws/pcc10"
 
 
--- phrs-modules/phrs-soap-pcc10ws/start-pcc10-endpoint.bat
+++ phrs-modules/phrs-soap-pcc10ws/start-pcc10-endpoint.bat
@@ -2,6 +2,4 @@ cls
 
 call mvn clean compile
 call mvn test-compile
-call mvn exec:java -Dexec.mainClass=at.srfg.kmt.ehealth.phrs.ws.soap.PCC9EndpointExample -Dexec.classpathScope=test -Dexec.args="localhost 8989 testws/pcc9"
-
-
+call mvn exec:java -Dexec.mainClass=at.srfg.kmt.ehealth.phrs.ws.soap.pcc10.PCC10EndpointExample -Dexec.classpathScope=test -Dexec.args="localhost 8989 testws/pcc10"
