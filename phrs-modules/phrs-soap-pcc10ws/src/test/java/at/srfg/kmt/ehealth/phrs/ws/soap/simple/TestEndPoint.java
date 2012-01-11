2875ca1d384e7da72a62e539a40e9c77aaec0678
mopuffus <mopuffus@yahoo.com>
2012-01-11 10:36:31 +0100

    java doc update

---
+++ phrs-modules/phrs-soap-pcc10ws/src/test/java/at/srfg/kmt/ehealth/phrs/ws/soap/simple/TestEndPoint.java
@@ -0,0 +1,26 @@
+/*
+ * Project :iCardea
+ * File : TestEndPoint.java
+ * Encoding : UTF-8
+ * Date : Dec 9, 2011
+ * User : Mihai Radulescu
+ */
+package at.srfg.kmt.ehealth.phrs.ws.soap.simple;
+
+import at.srfg.kmt.ehealth.phrs.ws.soap.pcc10.QUPCAR004030UVWebService;
+import at.srfg.kmt.ehealth.phrs.ws.soap.pcc10.QUPCAR004030UVWebService;
+import javax.xml.ws.Endpoint;
+
+/**
+ * mvn exec:java -Dexec.mainClass=at.srfg.kmt.ehealth.phrs.dataexchange.ihe.TestEndPoint -Dexec.classpathScope=test<br/>
+ * @author mihai
+ * @version 1.0-SNAPSHOT
+ * @since 1.0-SNAPSHOT
+ */
+public class TestEndPoint {
+
+    public static void main(String ...arg) {
+        System.out.println("End point starts");
+        Endpoint.publish("http://localhost:8989/testws", new QUPCAR004030UVWebService());
+    }
+}
---
+++ phrs-modules/phrs-soap-pcc10ws/src/test/java/at/srfg/kmt/ehealth/phrs/ws/soap/simple/TestServiceImpl.java
@@ -0,0 +1,29 @@
+/*
+ * Project :iCardea
+ * File : TestServiceImpl.java
+ * Encoding : UTF-8
+ * Date : Dec 9, 2011
+ * User : Mihai Radulescu
+ */
+package at.srfg.kmt.ehealth.phrs.ws.soap.simple;
+
+import javax.jws.WebService;
+
+/**
+ *
+ * @author mihai
+ * @version 1.0-SNAPSHOT
+ * @since 1.0-SNAPSHOT
+ */
+@WebService(endpointInterface="at.srfg.kmt.ehealth.phrs.dataexchange.ihe.TestService")
+public class TestServiceImpl implements  TestService {
+
+    @Override
+    public void doStuff() {
+        System.out.println("HERE");
+        System.out.println("HERE");
+        System.out.println("HERE");
+        System.out.println("HERE");
+    }
+
+}
