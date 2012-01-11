b7fc9ae62cebac8145c4ea85bbb21fd9070b4b7b
mopuffus <mopuffus@yahoo.com>
2012-01-11 15:26:20 +0100

    I use the right URI

--- phrs-modules/phrs-soap-pcc10ws/send-pcc10-vital-sign.bash
+++ phrs-modules/phrs-soap-pcc10ws/send-pcc10-vital-sign.bash
@@ -6,4 +6,4 @@ clear
 mvn compile
 mvn test-compile
 
-mvn exec:java -Dexec.mainClass=at.srfg.kmt.ehealth.phrs.ws.soap.pcc10.SendVitalSignPCC10Message -Dexec.classpathScope=test -Dexec.args="http://localhost:8080/testPCC10ws"
+mvn exec:java -Dexec.mainClass=at.srfg.kmt.ehealth.phrs.ws.soap.pcc10.SendVitalSignPCC10Message -Dexec.classpathScope=test -Dexec.args="http://localhost:8989/testws/pcc10"
--- phrs-modules/phrs-soap-pcc10ws/send-pcc10-vital-sign.bat
+++ phrs-modules/phrs-soap-pcc10ws/send-pcc10-vital-sign.bat
@@ -3,4 +3,4 @@ cls
 call mvn compile
 call mvn test-compile
 
-call mvn exec:java -Dexec.mainClass=at.srfg.kmt.ehealth.phrs.ws.soap.pcc10.SendVitalSignPCC10Message -Dexec.classpathScope=test -Dexec.args="http://localhost:8080/testPCC10ws"
+call mvn exec:java -Dexec.mainClass=at.srfg.kmt.ehealth.phrs.ws.soap.pcc10.SendVitalSignPCC10Message -Dexec.classpathScope=test -Dexec.args="http://localhost:8989/testws/pcc10"
