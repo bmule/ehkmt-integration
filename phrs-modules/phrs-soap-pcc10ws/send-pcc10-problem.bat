f31158566f59a229f7981522c26bc94fe9ed9d9d
mopuffus <mopuffus@yahoo.com>
2012-01-11 15:53:49 +0100

    the very first try

---
+++ phrs-modules/phrs-soap-pcc10ws/send-pcc10-problem.bat
@@ -0,0 +1,6 @@
+cls
+
+call mvn compile
+call mvn test-compile
+
+call mvn exec:java -Dexec.mainClass=at.srfg.kmt.ehealth.phrs.ws.soap.pcc10.ProblemClientFeverSymptomExample -Dexec.classpathScope=test -Dexec.args="http://localhost:8989/testws/pcc10"
---
+++ phrs-modules/phrs-soap-pcc10ws/send-pcc10-symptom.bat
@@ -0,0 +1,7 @@
+cls
+
+
+call mvn compile
+call mvn test-compile
+
+call mvn exec:java -Dexec.mainClass=at.srfg.kmt.ehealth.phrs.ws.soap.pcc10.SendProblemFatiquePCC10Message -Dexec.classpathScope=test -Dexec.args="http://localhost:8989/testws/pcc10"
