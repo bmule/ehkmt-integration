9a49c0f09823246fe80f8f68a87e14b59c7ef7c9
mopuffus <mopuffus@yahoo.com>
2012-01-11 11:36:18 +0100

    the handle package changes

---
+++ phrs-modules/phrs-soap-pcc10ws/src/main/java/at/srfg/kmt/ehealth/phrs/ws/soap/pcc10/DebugHandler.java
@@ -0,0 +1,161 @@
+/*
+ * Project :iCardea
+ * File : DebugHandler.java
+ * Encoding : UTF-8
+ * Date : Dec 21, 2011
+ * User : Mihai Radulescu
+ */
+package at.srfg.kmt.ehealth.phrs.ws.soap.pcc10;
+
+
+import java.net.MalformedURLException;
+import java.util.Iterator;
+import java.util.Set;
+import javax.xml.namespace.QName;
+import javax.xml.soap.SOAPBody;
+import javax.xml.soap.SOAPHeader;
+import javax.xml.soap.SOAPMessage;
+import javax.xml.ws.handler.MessageContext;
+import javax.xml.ws.handler.soap.SOAPHandler;
+import javax.xml.ws.handler.soap.SOAPMessageContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * This SOAP handler is used to used to display in a human readable form 
+ * the SOAP message parts (header and body). <br/>
+ * This SOAP handler is logging the SOAP message parts (header and body) using
+ * the underlying logging frame-work. <br/>
+ * This class was not designed to be extended.
+ * 
+ * @author Mihai
+ * @version 0.1
+ * @since 0.1
+ */
+public final class DebugHandler implements SOAPHandler<SOAPMessageContext> {
+
+    /**
+     * The Logger instance. All log messages from this class are routed through
+     * this member. The Logger name space is
+     * <code>at.srfg.kmt.ehealth.phrs.ws.soap.DebugHandler</code>.
+     */
+    private static final Logger LOGGER =
+            LoggerFactory.getLogger(DebugHandler.class);
+
+    /**
+     * Builds a <code>DebugHandler</code> instance.
+     *
+     */
+    public DebugHandler() throws MalformedURLException {
+        // UNIMPLEMENTED
+    }
+
+    /**
+     * This method returns null, always.
+     * 
+     * @return returns null, always.
+     */
+    @Override
+    public Set<QName> getHeaders() {
+        return null;
+    }
+
+    /**
+     * This method is not implemented.
+     * 
+     * @param mc not used, the method is not implemented.
+     */
+    @Override
+    public void close(MessageContext mc) {
+        // UNIMPLEMENTED
+    }
+
+    /**
+     * This method return true, always.
+     *
+     * @param c not used.
+     * @return it return true, always.
+     */
+    @Override
+    public boolean handleFault(SOAPMessageContext c) {
+        return true;
+    }
+
+    /**
+     * Logs the given SOAP message.
+     * 
+     * @param context the SOAP message to log.
+     * @return true, always.
+     */
+    @Override
+    public boolean handleMessage(SOAPMessageContext context) {
+        final SOAPMessage message = context.getMessage();
+        try {
+            final SOAPHeader header = message.getSOAPHeader();
+            process(header);
+            final SOAPBody body = message.getSOAPBody();
+            process(body);
+        } catch (Exception exception) {
+            LOGGER.error("Error during the SAOP message processing.");
+            LOGGER.error(exception.getMessage(), exception);
+        }
+
+        LOGGER.debug("handleMessage {}", message.toString());
+        return true;
+    }
+
+    /**
+     * Logs a SOAP message header. if the message is null then this method has 
+     * no effect (it only logs the null message).
+     * 
+     * @param header the SOAP level to log.
+     */
+    private void process(SOAPHeader header) {
+
+        if (header == null) {
+            LOGGER.debug("SOAP Header is null nothing to process");
+            return;
+        }
+
+        final Iterator childElements = header.examineAllHeaderElements();
+        if (!childElements.hasNext()) {
+            LOGGER.debug("No SOAP Header to process");
+            return;
+        }
+
+        try {
+            final String headerToString = Util.toString(header);
+            LOGGER.debug("The SOAP header to precess is : {}", headerToString);
+        } catch (Exception exception) {
+            LOGGER.error("The SOAP header can not be parsed.");
+            LOGGER.error(exception.getMessage(), exception);
+        }
+    }
+
+    /**
+     * Logs a SOAP message body.
+     * 
+     * @param body  the SOAP message body to be log log.
+     */
+    private void process(SOAPBody body) {
+        if (body == null) {
+            LOGGER.debug("SOAP Body null nothing to process");
+            return;
+        }
+
+        final Iterator childElements = body.getChildElements();
+        if (!childElements.hasNext()) {
+            LOGGER.debug("No Body to process");
+            return;
+        }
+
+        try {
+            final String bodyToString = Util.toString(body);
+            LOGGER.debug("The SOAP Body to precess is : {}", bodyToString);
+        } catch (Exception exception) {
+            LOGGER.error("The SOAP Body can not be parsed.");
+            LOGGER.error(exception.getMessage(), exception);
+        }
+        
+    }
+}
---
+++ phrs-modules/phrs-soap-pcc10ws/src/main/java/at/srfg/kmt/ehealth/phrs/ws/soap/pcc10/Util.java
@@ -0,0 +1,170 @@
+/*
+ * Project :iCardea
+ * File : Util.java
+ * Encoding : UTF-8
+ * Date : Dec 22, 2011
+ * User : Mihai Radulescu
+ */
+package at.srfg.kmt.ehealth.phrs.ws.soap.pcc10;
+
+
+import java.io.Serializable;
+import java.io.StringWriter;
+import java.util.List;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.*;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+import org.hl7.v3.CD;
+import org.hl7.v3.II;
+import org.hl7.v3.PN;
+import org.w3c.dom.Node;
+
+
+/**
+ * Contains a set of common purposed methods related with the SOAP message.
+ * <br/> This class is not designed to be extend.
+ *
+ * @author Miahai
+ * @version 0.1
+ * @since 0.1
+ */
+final class Util {
+
+    /**
+     * Don't let anyone to instantiate this class.
+     */
+    private Util() {
+        // UNIMPLEMENTED
+    }
+
+    /**
+     * Serialize a
+     * <code>org.w3c.dom.Node</code> in to a string, this string represents a
+     * well formated XML. This methods can be applied on the SOPA header and
+     * SOAP body (because both are extending the
+     * <code>org.w3c.dom.Node</code>) node interface.
+     *
+     *
+     * @param node the note to be serialize, it can not be null.
+     * @return
+     * @throws ParserConfigurationException if the header can not be (XML)
+     * parsed.
+     * @throws TransformerConfigurationException by any XML transformation
+     * related error.
+     * @throws TransformerException by any XML transformation related error.
+     * @throws NullPointerException if the
+     * <code>header</code> argument is null.
+     */
+    static String toString(Node node) throws
+            ParserConfigurationException, TransformerConfigurationException,
+            TransformerException {
+
+        if (node == null) {
+            throw new NullPointerException("The node argument can not be null.");
+        }
+
+        final StringWriter stringWriter = new StringWriter();
+        final StreamResult streamResult = new StreamResult(stringWriter);
+        final TransformerFactory transformerFactory = TransformerFactory.newInstance();
+        final Transformer transformer = transformerFactory.newTransformer();
+        transformer.setOutputProperty(OutputKeys.INDENT, "yes");
+        transformer.setOutputProperty("{http://xml.apache.org/xslt}indent-amount", "4");
+        transformer.setOutputProperty(OutputKeys.METHOD, "xml");
+        transformer.transform(new DOMSource(node), streamResult);
+
+        final String result = stringWriter.toString();
+        return result;
+    }
+
+    /**
+     * Transforms a
+     * <code>org.hl7.v3.II</code> in to a human readable string.
+     *
+     * @param ii the involved
+     * <code>org.hl7.v3.II</code> instance.
+     * @return a human readable string for the given instance.
+     */
+    static String toString(II ii) {
+        final StringBuffer result = new StringBuffer();
+        final String extension = ii.getExtension();
+
+        if (extension != null) {
+            result.append("Extension (UUID) : ");
+            result.append(extension);
+        }
+
+        return result.toString();
+    }
+
+    /**
+     * Transforms a
+     * <code>org.hl7.v3.CD</code> in to a human readable string.
+     *
+     * @param cd the involved
+     * <code>org.hl7.v3.CD</code> instance.
+     * @return a human readable string for the given instance.
+     */
+    static String toString(CD cd) {
+        final StringBuffer result = new StringBuffer();
+
+        if (cd.getCode() != null) {
+            result.append("Code : ");
+            result.append(cd.getCode());
+            result.append(", ");
+        }
+
+        if (cd.getCodeSystem() != null) {
+            result.append("CodeSystem : ");
+            result.append(cd.getCodeSystem());
+            result.append(", ");
+        }
+
+
+        if (cd.getCodeSystemName() != null) {
+            result.append("CodeSystemName : ");
+            result.append(cd.getCodeSystemName());
+            result.append(", ");
+        }
+
+        if (cd.getCodeSystemVersion() != null) {
+            result.append("CodeSystemVersion : ");
+            result.append(cd.getCodeSystemVersion());
+            result.append(", ");
+        }
+
+        final int length = result.length();
+        if (length > 2) {
+            result.delete(length - 2, length - 1);
+        }
+
+        return result.toString();
+    }
+
+    /**
+     * Transforms a
+     * <code>org.hl7.v3.PN</code> in to a human readable string.
+     *
+     * @param pn the involved
+     * <code>org.hl7.v3.PN</code> instance.
+     * @return a human readable string for the given instance.
+     */
+    static String toString(PN pn) {
+        final StringBuffer result = new StringBuffer();
+        final List<Serializable> content = pn.getContent();
+        if (content.isEmpty()) {
+            return "No Content Found.";
+        }
+
+        for (Serializable item : content) {
+            result.append(item);
+            result.append(", ");
+        }
+        final int length = result.length();
+        if (length > 2) {
+            result.delete(length - 2, length - 1);
+        }
+
+        return result.toString();
+    }
+}
