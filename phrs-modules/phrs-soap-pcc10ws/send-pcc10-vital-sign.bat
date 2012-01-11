72ce54391bfd6fc881c9258c5c3b8c02a679cc07
mopuffus <mopuffus@yahoo.com>
2012-01-11 15:09:21 +0100

    the very first try

---
+++ phrs-modules/phrs-soap-pcc10ws/send-pcc10-vital-sign.bat
@@ -0,0 +1,6 @@
+cls
+
+call mvn compile
+call mvn test-compile
+
+call mvn exec:java -Dexec.mainClass=at.srfg.kmt.ehealth.phrs.ws.soap.pcc10.SendVitalSignPCC10Message -Dexec.classpathScope=test -Dexec.args="http://localhost:8080/testPCC10ws"
