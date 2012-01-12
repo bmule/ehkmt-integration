6bb0abd5246cde4d1a7a9feb098762c3f929b401
mopuffus <mradules@kis30.salzburgresearch.at>
2012-01-12 09:47:58 +0100

    the very frist try

---
+++ etc/install-to-maven.bat
@@ -0,0 +1,10 @@
+cls
+
+echo on
+
+call mvn install:install-file  -Dfile=QUPC_AR004040UV_Service.jar  -Dsource=QUPC_AR004040UV_Service-sources.jar -DgroupId=org.hl7.v3 -DartifactId=QUPC_AR004040UV_Service -Dversion=0.1 -Dpackaging=jar
+echo "QUPC_AR004040UV_Service was install"
+
+call mvn install:install-file  -Dfile=QUPC_AR004030UV_Service.jar  -Dsource=QUPC_AR004030UV_Service-sources.jar -DgroupId=org.hl7.v3 -DartifactId=QUPC_AR004030UV_Service -Dversion=0.1 -Dpackaging=jar
+
+echo "QUPC_AR00400UV_Service was install"
