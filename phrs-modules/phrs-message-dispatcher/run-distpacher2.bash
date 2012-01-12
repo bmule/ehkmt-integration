f375d331ffa9882a62ee8e32d3d1793651357c55
mopuffus <mopuffus@yahoo.com>
2012-01-12 13:18:20 +0100

    no more deps on pcc09 or pcc10

---
+++ phrs-modules/phrs-message-dispatcher/run-distpacher2.bash
@@ -0,0 +1,9 @@
+#!/bin/bash
+#set -xv
+
+clear
+
+mvn compile
+mvn test-compile
+
+mvn exec:java -Dexec.mainClass=at.srfg.kmt.ehealth.phrs.dispatch.impl.RunSecondDispatcher -Dexec.classpathScope=test
