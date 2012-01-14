301f9c9a115c500eb5dc902b52bfac9ee93069b2
mopuffus <mopuffus@yahoo.com>
2012-01-14 17:28:45 +0100

    corrections

---
+++ phrs-modules/phrs-soap-pcc09ws/start-securepcc9-endpoint.bash
@@ -0,0 +1,10 @@
+#!/bin/bash
+#set -xv
+
+clear
+
+mvn clean compile
+mvn test-compile
+mvn exec:java -Dexec.mainClass=at.srfg.kmt.ehealth.phrs.ws.soap.pcc9.PCC9SecureEndpointExample -Dexec.classpathScope=test -Dexec.args="localhost 8089 testws/pcc9" -X
+
+
---
+++ phrs-modules/phrs-soap-pcc09ws/start-securepcc9-endpoint.bat
@@ -0,0 +1,7 @@
+cls
+
+call mvn clean compile
+call mvn test-compile
+call mvn exec:java -Dexec.mainClass=at.srfg.kmt.ehealth.phrs.ws.soap.pcc9.PCC9SecureEndpointExample -Dexec.classpathScope=test -Dexec.args="localhost 8089 testws/pcc9" -X
+
+
