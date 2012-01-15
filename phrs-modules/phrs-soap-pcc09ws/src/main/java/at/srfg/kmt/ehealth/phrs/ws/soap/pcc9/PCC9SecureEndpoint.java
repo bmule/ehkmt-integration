3ab298998968479bf3ca4c0f054cc1567c141c58
mihai <mihai@new-host.home>
2012-01-15 14:10:49 +0100

    better logging for the exception

--- phrs-modules/phrs-soap-pcc09ws/src/main/java/at/srfg/kmt/ehealth/phrs/ws/soap/pcc9/PCC9SecureEndpoint.java
+++ phrs-modules/phrs-soap-pcc09ws/src/main/java/at/srfg/kmt/ehealth/phrs/ws/soap/pcc9/PCC9SecureEndpoint.java
@@ -11,12 +11,11 @@ package at.srfg.kmt.ehealth.phrs.ws.soap.pcc9;
 import com.sun.net.httpserver.HttpContext;
 import com.sun.net.httpserver.HttpsConfigurator;
 import com.sun.net.httpserver.HttpsServer;
+import java.io.IOException;
 import java.io.InputStream;
 import java.net.InetSocketAddress;
-import java.security.KeyStore;
-import java.security.KeyStoreException;
-import java.security.NoSuchAlgorithmException;
-import java.security.SecureRandom;
+import java.security.*;
+import java.security.cert.CertificateException;
 import java.util.logging.Level;
 import javax.net.ssl.KeyManagerFactory;
 import javax.net.ssl.SSLContext;
@@ -107,7 +106,7 @@ public class PCC9SecureEndpoint {
     }
 
     private void start(String host, int port, String context,
-            String keysoreFile, String keyStorePasswd) throws Exception {
+            String keysoreFile, String keyStorePasswd) throws NoSuchAlgorithmException, KeyStoreException, IOException, CertificateException, UnrecoverableKeyException, KeyManagementException {
 
         final SSLContext ssl;
         try {
@@ -135,18 +134,36 @@ public class PCC9SecureEndpoint {
                     String.format("The key store file named %s can not be located in the classpath.", keysoreFile);
             final NullPointerException exception = new NullPointerException(msg);
             LOGGER.error(exception.getMessage(), exception);
+            throw exception;
+        }
+        try {
+            store.load(stream, keyStorePasswd.toCharArray());
+        } catch (IOException exception) {
+            LOGGER.error(exception.getMessage(), exception);
+            throw exception;
+        } catch (CertificateException exception) {
+            LOGGER.error(exception.getMessage(), exception);
+            throw exception;
         }
 
-        store.load(stream, keyStorePasswd.toCharArray());
-        keyFactory.init(store, keyStorePasswd.toCharArray());
+        try {
+            keyFactory.init(store, keyStorePasswd.toCharArray());
+        } catch (UnrecoverableKeyException exception) {
+            LOGGER.error(exception.getMessage(), exception);
+            throw exception;
+        }
 
         final TrustManagerFactory trustFactory =
                 TrustManagerFactory.getInstance(TrustManagerFactory.getDefaultAlgorithm());
 
         trustFactory.init(store);
-
-        ssl.init(keyFactory.getKeyManagers(),
-                trustFactory.getTrustManagers(), new SecureRandom());
+        try {
+            ssl.init(keyFactory.getKeyManagers(),
+                    trustFactory.getTrustManagers(), new SecureRandom());
+        } catch (KeyManagementException exception) {
+            LOGGER.error(exception.getMessage(), exception);
+            throw exception;
+        }
 
         final HttpsConfigurator configurator = new HttpsConfigurator(ssl);
         final HttpsServer httpsServer = HttpsServer.create(new InetSocketAddress(host, port), port);
