36a8a90aa665066f228c9c3d5b7c5f338a6bb87e
mihai <mihai@new-host.home>
2012-01-15 15:52:51 +0100

    file and file password is not pass over argumetns.

--- phrs-modules/phrs-soap-pcc09ws/start-securepcc9-endpoint.bash
+++ phrs-modules/phrs-soap-pcc09ws/start-securepcc9-endpoint.bash
@@ -5,6 +5,6 @@ clear
 
 mvn clean compile
 mvn test-compile
-mvn exec:java -Dexec.mainClass=at.srfg.kmt.ehealth.phrs.ws.soap.pcc9.PCC9SecureEndpointExample -Dexec.classpathScope=test -Dexec.args="localhost 8089 testws/pcc9"
+mvn exec:java -Dexec.mainClass=at.srfg.kmt.ehealth.phrs.ws.soap.pcc9.PCC9SecureEndpointExample -Dexec.classpathScope=test -Dexec.args="localhost 8089 testws/pcc9 srfg-phrs-core-keystore.ks icardea"
 
 
--- phrs-modules/phrs-soap-pcc09ws/start-securepcc9-endpoint.bat
+++ phrs-modules/phrs-soap-pcc09ws/start-securepcc9-endpoint.bat
@@ -2,6 +2,6 @@ cls
 
 call mvn clean compile
 call mvn test-compile
-call mvn exec:java -Dexec.mainClass=at.srfg.kmt.ehealth.phrs.ws.soap.pcc9.PCC9SecureEndpointExample -Dexec.classpathScope=test -Dexec.args="localhost 8089 testws/pcc9" -X
+call mvn exec:java -Dexec.mainClass=at.srfg.kmt.ehealth.phrs.ws.soap.pcc9.PCC9SecureEndpointExample -Dexec.classpathScope=test -Dexec.args="localhost 8089 testws/pcc9 srfg-phrs-core-keystore.ks icardea"
 
 
