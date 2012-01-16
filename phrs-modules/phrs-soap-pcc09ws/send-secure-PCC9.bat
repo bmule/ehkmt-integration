09b7a59ce3bdf46f394a2725bb5cf14029bb9b9e
mopuffus <mopuffus@yahoo.com>
2012-01-16 12:26:26 +0100

    I use the arguments to pass parameters

--- phrs-modules/phrs-soap-pcc09ws/send-secure-PCC9.bat
+++ phrs-modules/phrs-soap-pcc09ws/send-secure-PCC9.bat
@@ -3,4 +3,4 @@ cls
 call mvn compile
 call mvn test-compile
 
-mvn exec:java -Dexec.mainClass=at.srfg.kmt.ehealth.phrs.ws.soap.pcc9.SendSecurePcc09MessageExample -Dexec.classpathScope=test -Dexec.args="https://localhost:8089/testws/pcc9 http://localhost:8989/testws/pcc09"
+call mvn exec:java -Dexec.mainClass=at.srfg.kmt.ehealth.phrs.ws.soap.pcc9.SendSecurePcc09MessageExample -Dexec.classpathScope=test -Dexec.args="https://localhost:8089/testws/pcc9 https://localhost:8989/testws/pcc10  /Volumes/Data/lab0/iiiiiCardea/phrs/ehkmt-integration/etc/ssl/srfg-phrs-core-keystore.ks icardea"
