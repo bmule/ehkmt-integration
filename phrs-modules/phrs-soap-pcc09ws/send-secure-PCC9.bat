1850de6e7b08f1e69924a26b51ff9a3e9ad17cc4
mopuffus <mopuffus@yahoo.com>
2012-01-16 13:22:20 +0100

    windows like path

--- phrs-modules/phrs-soap-pcc09ws/send-secure-PCC9.bat
+++ phrs-modules/phrs-soap-pcc09ws/send-secure-PCC9.bat
@@ -3,4 +3,4 @@ cls
 call mvn compile
 call mvn test-compile
 
-call mvn exec:java -Dexec.mainClass=at.srfg.kmt.ehealth.phrs.ws.soap.pcc9.SendSecurePcc09MessageExample -Dexec.classpathScope=test -Dexec.args="https://localhost:8089/testws/pcc9 https://localhost:8989/testws/pcc10  /Volumes/Data/lab0/iiiiiCardea/phrs/ehkmt-integration/etc/ssl/srfg-phrs-core-keystore.ks icardea"
+mvn exec:java -Dexec.mainClass=at.srfg.kmt.ehealth.phrs.ws.soap.pcc9.SendSecurePcc09MessageExample -Dexec.classpathScope=test -Dexec.args="https://localhost:8089/testws/pcc9 https://localhost:8989/testws/pcc10  d:\\srfg-phrs-core-keystore.ks icardea"
