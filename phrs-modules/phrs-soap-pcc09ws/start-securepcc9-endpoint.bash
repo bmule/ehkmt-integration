13d4a69d57eee200e98e006017bb9d589ff06a97
mopuffus <mopuffus@yahoo.com>
2012-01-17 11:39:14 +0100

    exra comments

--- phrs-modules/phrs-soap-pcc09ws/start-securepcc9-endpoint.bash
+++ phrs-modules/phrs-soap-pcc09ws/start-securepcc9-endpoint.bash
@@ -5,6 +5,7 @@ clear
 
 mvn clean compile
 mvn test-compile
+# mvn exec:java -Dexec.mainClass=at.srfg.kmt.ehealth.phrs.ws.soap.pcc9.PCC9SecureEndpointExample -Dexec.classpathScope=test -Dexec.args="127.0.0.1 8089 testws/pcc9 local.ks simulator"
 mvn exec:java -Dexec.mainClass=at.srfg.kmt.ehealth.phrs.ws.soap.pcc9.PCC9SecureEndpointExample -Dexec.classpathScope=test -Dexec.args="localhost 8089 testws/pcc9 srfg-phrs-core-keystore.ks icardea"
 
 
