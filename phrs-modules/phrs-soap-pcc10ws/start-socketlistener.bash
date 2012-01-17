3739830695c6809df5bfff759e6c74010f45b48e
mopuffus <mopuffus@yahoo.com>
2012-01-17 18:24:35 +0100

    security over system properties

--- phrs-modules/phrs-soap-pcc10ws/start-socketlistener.bash
+++ phrs-modules/phrs-soap-pcc10ws/start-socketlistener.bash
@@ -5,6 +5,6 @@ clear
 
 mvn clean compile
 mvn test-compile
-mvn exec:java -Dexec.mainClass=at.srfg.kmt.ehealth.phrs.ws.soap.pcc10.StartSocketListener -Dexec.classpathScope=test
+mvn exec:java -Dexec.mainClass=at.srfg.kmt.ehealth.phrs.ws.soap.pcc10.StartSocketListener -Dexec.classpathScope=test -Djava.protocol.handler.pkgs=com.sun.net.ssl.internal.www.protocol -Djavax.net.ssl.trustStore=dddd -Djavax.net.ssl.trustStorePassword=icardea
 
 
--- phrs-modules/phrs-soap-pcc10ws/start-socketlistener.bat
+++ phrs-modules/phrs-soap-pcc10ws/start-socketlistener.bat
@@ -2,6 +2,6 @@ cls
 
 call mvn clean compile
 call mvn test-compile
-call mvn exec:java -Dexec.mainClass=at.srfg.kmt.ehealth.phrs.ws.soap.pcc10.StartSocketListener -Dexec.classpathScope=test -Dexec.args="icardea-server.lksdom21.lks.local icardea"
+call mvn exec:java -Dexec.mainClass=at.srfg.kmt.ehealth.phrs.ws.soap.pcc10.StartSocketListener -Dexec.classpathScope=test -Djava.protocol.handler.pkgs=com.sun.net.ssl.internal.www.protocol -Djavax.net.ssl.trustStore=dddd -Djavax.net.ssl.trustStorePassword=icardea
 
 
