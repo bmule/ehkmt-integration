1bd3cbba49adc77e3acb09e55abb0b035e769464
mopuffus <mopuffus@yahoo.com>
2012-01-17 11:38:48 +0100

    exra comments

--- phrs-modules/phrs-soap-pcc09ws/send-secure-PCC9.bash
+++ phrs-modules/phrs-soap-pcc09ws/send-secure-PCC9.bash
@@ -7,3 +7,4 @@ mvn compile
 mvn test-compile
 
 mvn exec:java -Dexec.mainClass=at.srfg.kmt.ehealth.phrs.ws.soap.pcc9.SendSecurePcc09MessageExample -Dexec.classpathScope=test -Dexec.args="https://localhost:8089/testws/pcc9 https://localhost:8989/testws/pcc10  /Volumes/Data/lab0/iiiiiCardea/phrs/ehkmt-integration/etc/ssl/srfg-phrs-core-keystore.ks icardea"
+# mvn exec:java -Dexec.mainClass=at.srfg.kmt.ehealth.phrs.ws.soap.pcc9.SendSecurePcc09MessageExample -Dexec.classpathScope=test -Dexec.args="https://localhost:8089/testws/pcc9 https://localhost:8989/testws/pcc10 src/test/resources/local.ks simulator"
