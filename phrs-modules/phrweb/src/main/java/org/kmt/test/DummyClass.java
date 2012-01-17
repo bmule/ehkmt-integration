99d20b73d23bdf5ceb762ade16408948439e38aa
bmule <bmulreni@salzburgresearch.at>
2012-01-17 11:56:17 +0100

    POM updates

--- phrs-modules/phrweb/src/main/java/org/kmt/test/DummyClass.java
+++ phrs-modules/phrweb/src/main/java/org/kmt/test/DummyClass.java
@@ -1,21 +1,9 @@
-/*
- * Project  :iCardea
- * File     : DummyClass.java
- * Encoding : UTF-8
- * Date     : Dec 21, 2011
- * User     : Mihai Radulescu
- */
+
 
 
 package org.kmt.test;
 
 
-/**
- *
- * @author m1s
- * @version 0.1
- * @since 0.1
- */
 public class DummyClass {
 
 }
