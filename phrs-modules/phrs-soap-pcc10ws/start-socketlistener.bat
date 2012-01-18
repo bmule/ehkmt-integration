aafa5b473b805ba27e1ceb3c5e6e32f8e6eae9e4
mopuffus <mopuffus@yahoo.com>
2012-01-18 11:14:18 +0100

    I use an other keystore file

--- phrs-modules/phrs-soap-pcc10ws/start-socketlistener.bat
+++ phrs-modules/phrs-soap-pcc10ws/start-socketlistener.bat
@@ -2,6 +2,6 @@ cls
 
 call mvn clean compile
 call mvn test-compile
-call mvn exec:java -Dexec.mainClass=at.srfg.kmt.ehealth.phrs.ws.soap.pcc10.StartSocketListener -Dexec.classpathScope=test -Djava.protocol.handler.pkgs=com.sun.net.ssl.internal.www.protocol -Djavax.net.ssl.trustStore=dddd -Djavax.net.ssl.trustStorePassword=icardea
+call mvn exec:java -Dexec.mainClass=at.srfg.kmt.ehealth.phrs.ws.soap.pcc10.StartSocketListener -Dexec.classpathScope=test -Djava.protocol.handler.pkgs=com.sun.net.ssl.internal.www.protocol -Djavax.net.ssl.trustStore=D:\\srdc\\codes\\icardea-google\\icardea\\icardea-caremanager-ws\\src\\test\\resources -Djavax.net.ssl.trustStorePassword=srdcpass
 
 
