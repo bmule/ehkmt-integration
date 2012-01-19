d5e8facef9022bb39fde185a1de25005cb540cfb
mopuffus <mopuffus@yahoo.com>
2012-01-19 14:08:26 +0100

    less spaces

--- phrs-modules/phrs-soap-pcc10ws/problem-client-example.bat
+++ phrs-modules/phrs-soap-pcc10ws/problem-client-example.bat
@@ -3,5 +3,3 @@ cls
 call mvn clean compile
 call mvn test-compile
 call mvn exec:java -Dexec.mainClass=at.srfg.kmt.ehealth.phrs.ws.soap.pcc10.ProblemClientFeverSymptomExample -Dexec.classpathScope=test
-
-
