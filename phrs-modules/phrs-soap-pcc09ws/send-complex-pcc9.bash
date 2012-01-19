889a716b5f048be7f5386f4112b2383593d2bce0
mopuffus <mopuffus@yahoo.com>
2012-01-19 13:49:21 +0100

    proper names

--- phrs-modules/phrs-soap-pcc09ws/send-complex-pcc9.bash
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-#set -xv
-
-clear
-
-mvn compile
-mvn test-compile
-
-mvn exec:java -Dexec.mainClass=at.srfg.kmt.ehealth.phrs.ws.soap.pcc9.SendComplexPcc09MessageExample -Dexec.classpathScope=test -Dexec.args="http://localhost:8089/testws/pcc9 http://localhost:8989/testws/pcc10 COBSCAT"
