8d07d38b6c28b9bb317be488a848568c10ce9272
mihai <mihai@new-host.home>
2012-01-23 14:04:17 +0100

    medlist messages

---
+++ phrs-modules/phrs-soap-pcc09ws/send-medlist-pcc9.bat
@@ -0,0 +1,6 @@
+cls
+
+call mvn compile
+call mvn test-compile
+
+call mvn exec:java -Dexec.mainClass=at.srfg.kmt.ehealth.phrs.ws.soap.pcc9.SendComplexPcc09MessageExample -Dexec.classpathScope=test -Dexec.args="http://localhost:8089/testws/pcc9 http://localhost:8989/testws/pcc10 MEDLIST"
