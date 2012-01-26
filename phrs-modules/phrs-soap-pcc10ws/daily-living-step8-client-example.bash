c20bc853318e202ba08aa83c5514520a8bfcf842
fstroh <fstroh@gmail.com>
2012-01-26 17:19:42 +0100

    modified:   phrs-modules/phrs-constants/src/main/java/at/srfg/kmt/ehealth/phrs/Constants.java 	modified:   phrs-modules/phrs-dataexchange-client/src/main/resources/startup.core.rdf 	new file:   phrs-modules/phrs-soap-pcc10ws/daily-living-step8-client-example.bash 	new file:   phrs-modules/phrs-soap-pcc10ws/daily-living-step8-client-example.bat 	new file:   phrs-modules/phrs-soap-pcc10ws/src/test/java/at/srfg/kmt/ehealth/phrs/ws/soap/pcc10/DailyLivingStep8ClientExample.java

---
+++ phrs-modules/phrs-soap-pcc10ws/daily-living-step8-client-example.bash
@@ -0,0 +1,8 @@
+#!/bin/bash
+#set -xv
+
+clear
+
+mvn clean compile
+mvn test-compile
+mvn exec:java -Dexec.mainClass=at.srfg.kmt.ehealth.phrs.ws.soap.pcc10.DailyLivingStep8ClientExample -Dexec.classpathScope=test
