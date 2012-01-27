6105e7cf25cd6ba0b2937d7f769d490318d0a207
bmule <bmulreni@salzburgresearch.at>
2012-01-27 16:11:11 +0100

    tomcat java settings for memory

---
+++ etc/tomcat/setenv.bat
@@ -0,0 +1,3 @@
+rem Put into Tomcat /bin/setenv.bat
+
+set JAVA_OPTS=-Xms512m -Xmx1024m -XX:PermSize=256m -XX:MaxPermSize=512m
---
+++ etc/tomcat/setenv.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+# Put into Tomcat /bin/setenv.sh
+#
+
+export JAVA_OPTS="-Xms512m -Xmx1024m -XX:PermSize=256m -XX:MaxPermSize=512m"
