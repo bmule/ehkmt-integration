e78f050afc1ac71cd74ad8c92aff4db991984e07
mopuffus <mopuffus@yahoo.com>
2012-01-27 10:45:24 +0100

    the very first try

--- phrs-modules/phrs-dataexchange-client/src/test/java/at/srfg/kmt/ehealth/phrs/dataexchange/client/RepetiveResositoryCloseExample.java
+++ phrs-modules/phrs-dataexchange-client/src/test/java/at/srfg/kmt/ehealth/phrs/dataexchange/client/RepetiveResositoryCloseExample.java
@@ -15,13 +15,12 @@ import at.srfg.kmt.ehealth.phrs.persistence.api.GenericTriplestore;
 import at.srfg.kmt.ehealth.phrs.persistence.api.GenericTriplestoreLifecycle;
 import at.srfg.kmt.ehealth.phrs.persistence.api.TripleException;
 import at.srfg.kmt.ehealth.phrs.persistence.impl.TriplestoreConnectionFactory;
-import java.util.HashSet;
-import java.util.Set;
 import org.apache.commons.beanutils.DynaBean;
 
 
 /**
- * This examples open and close the Client connection to the its underlying.
+ * This examples open and close the Client connection to the its underlying
+ * store.
  *
  * @author M1s
  * @version 1.0-SNAPSHOT
@@ -35,11 +34,11 @@ public final class RepetiveResositoryCloseExample {
         final TriplestoreConnectionFactory connectionFactory =
                 TriplestoreConnectionFactory.getInstance();
         // Here I obtain a triplestore instance.
-        final GenericTriplestore triplestore = connectionFactory.getTriplestore();
+        GenericTriplestore triplestore = connectionFactory.getTriplestore();
 
         final String owner = "testOwner";
         // Here I build a a client for the connection
-        final ProblemEntryClient client = new ProblemEntryClient(triplestore);
+        ProblemEntryClient client = new ProblemEntryClient(triplestore);
 
         // Here I use the client
         client.addProblemEntry(
@@ -51,8 +50,9 @@ public final class RepetiveResositoryCloseExample {
                 "Free text note.",
                 Constants.HL7V3_FATIQUE);
 
-        final Iterable<String> uris = client.getProblemEntriesURIForUser(owner);
-        final DynaBeanClient dynaBeanClient = new DynaBeanClient(triplestore);
+        Iterable<String> uris = client.getProblemEntriesURIForUser(owner);
+        DynaBeanClient dynaBeanClient = new DynaBeanClient(triplestore);
+        System.out.println("First iteration");
         for (String uri : uris) {
             final DynaBean dynaBean = dynaBeanClient.getDynaBean(uri);
             final String toString = DynaBeanUtil.toString(dynaBean);
@@ -61,7 +61,28 @@ public final class RepetiveResositoryCloseExample {
 
         // Here I close the connection for the upper defined client.
         ((GenericTriplestoreLifecycle) triplestore).shutdown();
-pl    }
+        
+        triplestore = connectionFactory.getTriplestore();
+        
+        client = new ProblemEntryClient(triplestore);
 
-    
+        // Here I use the client closed before
+        client.addProblemEntry(
+                owner,
+                Constants.HL7V3_COMPILANT,
+                Constants.STATUS_COMPELETE,
+                "201007120000",
+                Constants.HL7V3_SYMPTOM,
+                "Free text note.",
+                Constants.HL7V3_FEVER);
+        
+        uris = client.getProblemEntriesURIForUser(owner);
+        dynaBeanClient = new DynaBeanClient(triplestore);
+        System.out.println("Second iteration");
+        for (String uri : uris) {
+            final DynaBean dynaBean = dynaBeanClient.getDynaBean(uri);
+            final String toString = DynaBeanUtil.toString(dynaBean);
+            System.out.println(toString);
+        }
+    }
 }
