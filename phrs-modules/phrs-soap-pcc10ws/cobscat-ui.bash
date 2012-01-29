1377c13e6c03dbed29f430f95a4deccb4b024105
mopuffus <mopuffus@yahoo.com>
2012-01-29 18:01:29 +0100

    the very frist try

---
+++ phrs-modules/phrs-soap-pcc10ws/cobscat-ui.bash
@@ -0,0 +1,10 @@
+#!/bin/bash
+#set -xv
+
+clear
+
+mvn clean compile
+mvn test-compile
+mvn exec:java -Dexec.mainClass=at.srfg.kmt.ehealth.phrs.ws.soap.pcc10.CobscatUI -Dexec.classpathScope=test
+
+
