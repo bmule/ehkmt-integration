b20c5ae230e3a7028f847a7e72849a283b56e57a
mopuffus <mopuffus@yahoo.com>
2012-02-07 23:17:56 +0100

    small corrections

--- phrs-modules/phrs-soap-pcc09ws/send-cobscat-secure-PCC9.bat
+++ phrs-modules/phrs-soap-pcc09ws/send-cobscat-secure-PCC9.bat
@@ -5,4 +5,3 @@ call mvn compile
 call mvn test-compile
 
 call mvn exec:java -Dexec.mainClass=at.srfg.kmt.ehealth.phrs.ws.soap.pcc9.SendSecurePcc09MessageExample -Dexec.classpathScope=test -Dexec.args="https://localhost:8089/testws/pcc9 https://localhost:8989/testws/pcc10  /Volumes/Data/lab0/iiiiiCardea/phrs/ehkmt-integration/etc/ssl/srfg-phrs-core-keystore.ks icardea MEDCCAT 198 Moppuffus Lumpkins M"
-# mvn exec:java -Dexec.mainClass=at.srfg.kmt.ehealth.phrs.ws.soap.pcc9.SendSecurePcc09MessageExample -Dexec.classpathScope=test -Dexec.args="https://localhost:8089/testws/pcc9 https://localhost:8989/testws/pcc10 src/test/resources/local.ks simulator"
