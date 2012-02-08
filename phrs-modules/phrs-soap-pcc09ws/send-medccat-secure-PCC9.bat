680d7eba8ccaa68d9460b6f6897f674e577231f8
mopuffus <mopuffus@yahoo.com>
2012-02-08 11:43:23 +0100

    the right Windows like path for the trust store

--- phrs-modules/phrs-soap-pcc09ws/send-medccat-secure-PCC9.bat
+++ phrs-modules/phrs-soap-pcc09ws/send-medccat-secure-PCC9.bat
@@ -3,5 +3,6 @@ cls
 call mvn compile
 call mvn test-compile
 
-call mvn exec:java -Dexec.mainClass=at.srfg.kmt.ehealth.phrs.ws.soap.pcc9.SendSecurePcc09MessageExample -Dexec.classpathScope=test -Dexec.args="https://localhost:8089/testws/pcc9 https://localhost:8989/testws/pcc10  /Volumes/Data/lab0/iiiiiCardea/phrs/ehkmt-integration/etc/ssl/srfg-phrs-core-keystore.ks icardea COBSCAT 191 Moppuffus Lumpkins M"
+call mvn exec:java -Dexec.mainClass=at.srfg.kmt.ehealth.phrs.ws.soap.pcc9.SendSecurePcc09MessageExample -Dexec.classpathScope=test -Dexec.args="https://localhost:8089/testws/pcc9 https://localhost:8989/testws/pcc10  D:\\srfg\\new-phrs\\icardea-phrs\\phrs-soap-pcc09ws\\src\\main\\resources\\srfg-phrs-core-keystore.ks icardea COBSCAT 191 Moppuffus Lumpkins M"
 
+pause
