a8b79d404854e6309157f8a6321d078506fd2e3e
mopuffus <mopuffus@yahoo.com>
2012-02-08 08:39:01 +0100

    I use the process system property

--- phrs-modules/phrs-soap-pcc10ws/start-pcc10-endpoint.bash
+++ phrs-modules/phrs-soap-pcc10ws/start-pcc10-endpoint.bash
@@ -1,10 +1,13 @@
-#!/bin/bash
+2#!/bin/bash
 #set -xv
 
 clear
 
 mvn clean compile
 mvn test-compile
-mvn exec:java -Dexec.mainClass=at.srfg.kmt.ehealth.phrs.ws.soap.pcc10.PCC10EndpointExample -Dexec.classpathScope=test -Dexec.args="localhost 8989 testws/pcc10"
+mvn exec:java -Dexec.mainClass=at.srfg.kmt.ehealth.phrs.ws.soap.pcc10.PCC10EndpointExample \
+-Dexec.classpathScope=test \
+-Dexec.args="localhost 8989 testws/pcc10" \
+-Dpcc10.process="true"
 
 
