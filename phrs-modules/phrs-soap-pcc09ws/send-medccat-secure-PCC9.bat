9d18a83575d3bd491a5f1aa1f8feaa9934dedab6
mopuffus <mradules@kis30.salzburgresearch.at>
2012-02-08 17:07:43 +0100

    I use the right host name (fro the SALK machine)

--- phrs-modules/phrs-soap-pcc09ws/send-medccat-secure-PCC9.bat
+++ phrs-modules/phrs-soap-pcc09ws/send-medccat-secure-PCC9.bat
@@ -3,6 +3,6 @@ cls
 call mvn compile
 call mvn test-compile
 
-call mvn exec:java -Dexec.mainClass=at.srfg.kmt.ehealth.phrs.ws.soap.pcc9.SendSecurePcc09MessageExample -Dexec.classpathScope=test -Dexec.args="https://localhost:8089/testws/pcc9 https://localhost:8989/testws/pcc10  D:\\srfg\\new-phrs\\icardea-phrs\\phrs-soap-pcc09ws\\src\\main\\resources\\srfg-phrs-core-keystore.ks icardea COBSCAT 191 Moppuffus Lumpkins M"
+call mvn exec:java -Dexec.mainClass=at.srfg.kmt.ehealth.phrs.ws.soap.pcc9.SendSecurePcc09MessageExample -Dexec.classpathScope=test -Dexec.args="https://icardea-server.lksdom21.lks.local:8089/testws/pcc9 https://localhost:8989/testws/pcc10  D:\\srfg\\new-phrs\\icardea-phrs\\phrs-soap-pcc09ws\\src\\main\\resources\\srfg-phrs-core-keystore.ks icardea COBSCAT 191 Moppuffus Lumpkins M"
 
 pause
