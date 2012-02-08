5d6a1caafb87a31d58fb892f484a01c172358a49
mopuffus <mopuffus@yahoo.com>
2012-02-08 11:19:23 +0100

    I use the right (VT Scenario) protocol id

--- phrs-modules/phrs-soap-pcc09ws/send-cobscat-pcc9.bash
+++ phrs-modules/phrs-soap-pcc09ws/send-cobscat-pcc9.bash
@@ -8,4 +8,4 @@ mvn test-compile
 
 mvn exec:java -Dexec.mainClass=at.srfg.kmt.ehealth.phrs.ws.soap.pcc9.SendComplexPcc09MessageExample \
  -Dexec.classpathScope=test  \
- -Dexec.args="http://localhost:8089/testws/pcc9 http://localhost:8989/testws/pcc10 COBSCAT 198 Moppuffus Lumpkins M"
+ -Dexec.args="http://localhost:8089/testws/pcc9 http://localhost:8989/testws/pcc10 COBSCAT 191 Moppuffus Lumpkins M"
--- phrs-modules/phrs-soap-pcc09ws/send-medccat-secure-PCC9.bat
+++ phrs-modules/phrs-soap-pcc09ws/send-medccat-secure-PCC9.bat
@@ -3,5 +3,5 @@ cls
 call mvn compile
 call mvn test-compile
 
-call mvn exec:java -Dexec.mainClass=at.srfg.kmt.ehealth.phrs.ws.soap.pcc9.SendSecurePcc09MessageExample -Dexec.classpathScope=test -Dexec.args="https://localhost:8089/testws/pcc9 https://localhost:8989/testws/pcc10  /Volumes/Data/lab0/iiiiiCardea/phrs/ehkmt-integration/etc/ssl/srfg-phrs-core-keystore.ks icardea COBSCAT 198 Moppuffus Lumpkins M"
+call mvn exec:java -Dexec.mainClass=at.srfg.kmt.ehealth.phrs.ws.soap.pcc9.SendSecurePcc09MessageExample -Dexec.classpathScope=test -Dexec.args="https://localhost:8089/testws/pcc9 https://localhost:8989/testws/pcc10  /Volumes/Data/lab0/iiiiiCardea/phrs/ehkmt-integration/etc/ssl/srfg-phrs-core-keystore.ks icardea COBSCAT 191 Moppuffus Lumpkins M"
 
