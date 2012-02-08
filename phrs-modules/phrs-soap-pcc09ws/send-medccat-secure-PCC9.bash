3c54a674e2893e63ac15157e80a10b36ae60040e
mopuffus <mopuffus@yahoo.com>
2012-02-08 17:18:11 +0100

    the right care provision code

--- phrs-modules/phrs-soap-pcc09ws/send-medccat-secure-PCC9.bash
+++ phrs-modules/phrs-soap-pcc09ws/send-medccat-secure-PCC9.bash
@@ -6,5 +6,7 @@ clear
 mvn compile
 mvn test-compile
 
-mvn exec:java -Dexec.mainClass=at.srfg.kmt.ehealth.phrs.ws.soap.pcc9.SendSecurePcc09MessageExample -Dexec.classpathScope=test -Dexec.args="https://localhost:8089/testws/pcc9 https://localhost:8989/testws/pcc10  /Volumes/Data/lab0/iiiiiCardea/phrs/ehkmt-integration/etc/ssl/srfg-phrs-core-keystore.ks icardea COBSCAT 191 Moppuffus Lumpkins M"
+mvn exec:java -Dexec.mainClass=at.srfg.kmt.ehealth.phrs.ws.soap.pcc9.SendSecurePcc09MessageExample \
+ -Dexec.classpathScope=test \
+ -Dexec.args="https://localhost:8089/testws/pcc9 https://localhost:8989/testws/pcc10  /Volumes/Data/lab0/iiiiiCardea/phrs/ehkmt-integration/etc/ssl/srfg-phrs-core-keystore.ks icardea MEDCCAT 191 Moppuffus Lumpkins M"
 # mvn exec:java -Dexec.mainClass=at.srfg.kmt.ehealth.phrs.ws.soap.pcc9.SendSecurePcc09MessageExample -Dexec.classpathScope=test -Dexec.args="https://localhost:8089/testws/pcc9 https://localhost:8989/testws/pcc10 src/test/resources/local.ks simulator"
