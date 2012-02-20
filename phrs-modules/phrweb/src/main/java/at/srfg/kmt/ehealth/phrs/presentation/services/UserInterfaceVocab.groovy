1c1100a9ab3eeeceb615f48d56b9e78468b15e4c
bmule <bmulreni@salzburgresearch.at>
2012-02-21 00:17:35 +0100

    remove

--- phrs-modules/phrweb/src/main/java/at/srfg/kmt/ehealth/phrs/presentation/services/UserInterfaceVocab.groovy
+++
@@ -1,9 +0,0 @@
-package at.srfg.kmt.ehealth.phrs.presentation.services
-
-public class  UserInterfaceVocab {
-	
-	List medicalContactRoleVocab = ['GP -general practitioner','physician','dentist','other']
-	List phoneTypeVocab = ['mobile','office','fax','other']
-	
-	//getters and setters for each by groovy.
-}
