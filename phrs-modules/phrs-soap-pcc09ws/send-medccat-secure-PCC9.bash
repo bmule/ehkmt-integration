04952ed3068f72a8242acc4744d610b1be4e6bbc
bmule <bmulreni@salzburgresearch.at>
2012-02-21 06:47:51 +0100

    updated patient name

--- phrs-modules/phrs-soap-pcc09ws/send-medccat-secure-PCC9.bash
+++ phrs-modules/phrs-soap-pcc09ws/send-medccat-secure-PCC9.bash
@@ -8,5 +8,5 @@ mvn test-compile
 
 mvn exec:java -Dexec.mainClass=at.srfg.kmt.ehealth.phrs.ws.soap.pcc9.SendSecurePcc09MessageExample \
  -Dexec.classpathScope=test \
- -Dexec.args="https://localhost:8089/testws/pcc9 https://localhost:8989/testws/pcc10  /Volumes/Data/lab0/iiiiiCardea/phrs/ehkmt-integration/etc/ssl/srfg-phrs-core-keystore.ks icardea MEDCCAT 191 Moppuffus Lumpkins M"
+ -Dexec.args="https://localhost:8089/testws/pcc9 https://localhost:8989/testws/pcc10  /Volumes/Data/lab0/iiiiiCardea/phrs/ehkmt-integration/etc/ssl/srfg-phrs-core-keystore.ks icardea MEDCCAT 191 Suzie Mayr F"
 # mvn exec:java -Dexec.mainClass=at.srfg.kmt.ehealth.phrs.ws.soap.pcc9.SendSecurePcc09MessageExample -Dexec.classpathScope=test -Dexec.args="https://localhost:8089/testws/pcc9 https://localhost:8989/testws/pcc10 src/test/resources/local.ks simulator"
--- phrs-modules/phrs-soap-pcc09ws/send-medlist-secure-PCC9.bat
+++ phrs-modules/phrs-soap-pcc09ws/send-medlist-secure-PCC9.bat
@@ -3,6 +3,6 @@ cls
 call mvn compile
 call mvn test-compile
 
-call mvn exec:java -Dexec.mainClass=at.srfg.kmt.ehealth.phrs.ws.soap.pcc9.SendSecurePcc09MessageExample -Dexec.classpathScope=test -Dexec.args="https://icardea-server.lksdom21.lks.local/ehrif/pcc/ https://icardea-server.lksdom21.lks.local:8989/testws/pcc10 D:\\srfg\\new-phrs\\icardea-phrs\\phrs-soap-pcc09ws\\src\\main\\resources\\srfg-phrs-core-keystore.ks icardea MEDLIST 191 Moppuffus Lumpkins M"
+call mvn exec:java -Dexec.mainClass=at.srfg.kmt.ehealth.phrs.ws.soap.pcc9.SendSecurePcc09MessageExample -Dexec.classpathScope=test -Dexec.args="https://icardea-server.lksdom21.lks.local/ehrif/pcc/ https://icardea-server.lksdom21.lks.local:8989/testws/pcc10 D:\\srfg\\new-phrs\\icardea-phrs\\phrs-soap-pcc09ws\\src\\main\\resources\\srfg-phrs-core-keystore.ks icardea MEDLIST 191 Suzie Mayr F"
 
 pause
