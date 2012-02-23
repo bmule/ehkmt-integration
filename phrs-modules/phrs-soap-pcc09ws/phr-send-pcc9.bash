5ab5a879b4dcc31f45edcf4335c4886a2997a168
bmule <bmulreni@salzburgresearch.at>
2012-02-23 14:08:48 +0100

    multiple messages for different codes

---
+++ phrs-modules/phrs-soap-pcc09ws/phr-send-pcc9.bash
@@ -0,0 +1,27 @@
+#!/bin/bash
+#set -xv
+
+clear
+
+mvn compile
+mvn test-compile
+
+mvn exec:java -Dexec.mainClass=at.srfg.kmt.ehealth.phrs.ws.soap.pcc9.SendComplexPcc09MessageExample \
+ -Dexec.classpathScope=test  \
+ -Dexec.args="http://localhost:8089/testws/pcc9 http://localhost:8989/testws/pcc10 COBSCAT 191 Suzie Mayr F"
+
+mvn exec:java -Dexec.mainClass=at.srfg.kmt.ehealth.phrs.ws.soap.pcc9.SendComplexPcc09MessageExample \
+ -Dexec.classpathScope=test \
+ -Dexec.args="http://localhost:8089/testws/pcc9 http://localhost:8989/testws/pcc10 MEDLIST 191 Suzie Mayr F"
+
+mvn exec:java -Dexec.mainClass=at.srfg.kmt.ehealth.phrs.ws.soap.pcc9.SendComplexPcc09MessageExample \
+ -Dexec.classpathScope=test \
+ -Dexec.args="http://localhost:8089/testws/pcc9 http://localhost:8989/testws/pcc10 MEDCCAT 191 Suzie Mayr F"
+
+mvn exec:java -Dexec.mainClass=at.srfg.kmt.ehealth.phrs.ws.soap.pcc9.SendComplexPcc09MessageExample \
+ -Dexec.classpathScope=test \
+ -Dexec.args="http://localhost:8089/testws/pcc9 http://localhost:8989/testws/pcc10 COBSCAT 111 Suzie Mayr F"
+
+mvn exec:java -Dexec.mainClass=at.srfg.kmt.ehealth.phrs.ws.soap.pcc9.SendComplexPcc09MessageExample \
+ -Dexec.classpathScope=test \
+ -Dexec.args="http://localhost:8089/testws/pcc9 http://localhost:8989/testws/pcc10 MEDLIST 121 Suzie Mayr F"
---
+++ phrs-modules/phrs-soap-pcc09ws/send-medcat-pcc9.bat
@@ -0,0 +1,8 @@
+call title Send Medlist pcc09
+cls
+call mvn compile
+call mvn test-compile
+
+call mvn exec:java -Dexec.mainClass=at.srfg.kmt.ehealth.phrs.ws.soap.pcc9.SendComplexPcc09MessageExample -Dexec.classpathScope=test -Dexec.args="http://localhost:8089/testws/pcc9 http://localhost:8989/testws/pcc10 MEDCCAT 191 Suzie Mayr F"
+
+pause
