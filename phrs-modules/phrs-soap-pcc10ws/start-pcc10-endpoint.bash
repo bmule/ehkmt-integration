b0dabd62a129c525af14b749a06f3d420f8c0370
bmule <bmulreni@salzburgresearch.at>
2012-03-03 09:21:42 +0100

    remove maven clean,compile in order to preserve the log file at startup

--- phrs-modules/phrs-soap-pcc10ws/start-pcc10-endpoint.bash
+++ phrs-modules/phrs-soap-pcc10ws/start-pcc10-endpoint.bash
@@ -3,8 +3,8 @@
 
 clear
 
-mvn clean compile
-mvn test-compile
+#mvn clean compile
+#mvn test-compile
 mvn exec:java -Dexec.mainClass=at.srfg.kmt.ehealth.phrs.ws.soap.pcc10.PCC10EndpointExample \
 -Dexec.classpathScope=test \
 -Dexec.args="localhost 8989 testws/pcc10" \
--- phrs-modules/phrs-soap-pcc10ws/start-socketlistener.bash
+++ phrs-modules/phrs-soap-pcc10ws/start-socketlistener.bash
@@ -3,8 +3,8 @@
 
 clear
 
-mvn clean compile
-mvn test-compile
+#mvn clean compile
+#mvn test-compile
 mvn exec:java -Dexec.mainClass=at.srfg.kmt.ehealth.phrs.ws.soap.pcc10.StartSocketListener -Dexec.classpathScope=test -Djava.protocol.handler.pkgs=com.sun.net.ssl.internal.www.protocol -Djavax.net.ssl.trustStore=dddd -Djavax.net.ssl.trustStorePassword=icardea
 
 
