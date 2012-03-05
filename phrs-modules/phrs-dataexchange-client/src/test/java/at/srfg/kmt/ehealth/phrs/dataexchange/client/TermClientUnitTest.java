907357fe0a0b2da69064507ff0fd28a8ec0a8e0f
mopuffus <mopuffus@yahoo.com>
2012-03-05 16:44:44 +0100

    I add the testGetTermURI test

--- phrs-modules/phrs-dataexchange-client/src/test/java/at/srfg/kmt/ehealth/phrs/dataexchange/client/TermClientUnitTest.java
+++ phrs-modules/phrs-dataexchange-client/src/test/java/at/srfg/kmt/ehealth/phrs/dataexchange/client/TermClientUnitTest.java
@@ -8,16 +8,15 @@
 package at.srfg.kmt.ehealth.phrs.dataexchange.client;
 
 
-import static org.junit.Assert.*;
 import at.srfg.kmt.ehealth.phrs.Constants;
-import at.srfg.kmt.ehealth.phrs.persistence.api.Triple;
-import at.srfg.kmt.ehealth.phrs.persistence.api.TripleException;
+import at.srfg.kmt.ehealth.phrs.dataexchange.util.DynaBeanUtil;
+import at.srfg.kmt.ehealth.phrs.persistence.api.*;
 import at.srfg.kmt.ehealth.phrs.persistence.impl.TriplestoreConnectionFactory;
-import org.junit.Before;
+import com.sun.org.apache.xalan.internal.xsltc.compiler.Template;
+import org.apache.commons.beanutils.DynaBean;
 import org.junit.After;
-import at.srfg.kmt.ehealth.phrs.persistence.api.GenericRepositoryException;
-import at.srfg.kmt.ehealth.phrs.persistence.api.GenericTriplestore;
-import at.srfg.kmt.ehealth.phrs.persistence.api.GenericTriplestoreLifecycle;
+import static org.junit.Assert.*;
+import org.junit.Before;
 import org.junit.Test;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -32,36 +31,44 @@ import org.slf4j.LoggerFactory;
 public class TermClientUnitTest {
 
     /**
-     * The Logger instance. All log messages from this class
-     * are routed through this member. The Logger name space
-     * is <code>TermClientUnitTest</code>.
+     * The Logger instance. All log messages from this class are routed through
+     * this member. The Logger name space is
+     * <code>TermClientUnitTest</code>.
      */
     private static final Logger LOGGER =
             LoggerFactory.getLogger(TermClientUnitTest.class);
+    
+    private static final String UMLS_CODE_SYSTEM_CODE = "2.16.840.1.113883.6.86";
+
 
     private GenericTriplestore triplestore;
+
     private TermClient termClient;
+    
+    private DynaBeanClient dynaBeanClient;
 
     /**
      * Runs before any test from this suite and prepare the environment for the
      * next running test.
-     * 
-     * @throws GenericRepositoryException if this occurs then the test 
+     *
+     * @throws GenericRepositoryException if this occurs then the test
      * environment may be wrong set.
      */
     @Before
     public void initSuite() throws GenericRepositoryException {
         final TriplestoreConnectionFactory connectionFactory =
                 TriplestoreConnectionFactory.getInstance();
-        triplestore = connectionFactory.getTriplestore();        
+        triplestore = connectionFactory.getTriplestore();
         termClient = new TermClient(triplestore);
+        dynaBeanClient = new DynaBeanClient(triplestore);
+        
     }
 
     /**
-     * Runs all after any test from this suite and cleans the environment in 
+     * Runs all after any test from this suite and cleans the environment in
      * order that the next test will get a 'clean' environment.
-     * 
-     * @throws GenericRepositoryException 
+     *
+     * @throws GenericRepositoryException
      */
     @After
     public void shutdownSuite() throws GenericRepositoryException {
@@ -69,18 +76,60 @@ public class TermClientUnitTest {
         ((GenericTriplestoreLifecycle) triplestore).cleanEnvironment();
         triplestore = null;
     }
-    
+
     @Test
     public void searchSymptomTerms() throws TripleException {
-        final Iterable<Triple> termsRelatedWith = 
+        final Iterable<Triple> termsRelatedWith =
                 termClient.getTermsRelatedWith(Constants.HL7V3_SYMPTOM);
         assertTrue(termsRelatedWith.iterator().hasNext());
     }
-    
+
     @Test
     public void searchConditionTerms() throws TripleException {
-        final Iterable<Triple> termsRelatedWith = 
+        final Iterable<Triple> termsRelatedWith =
                 termClient.getTermsRelatedWith(Constants.HL7V3_CONDITION);
         assertFalse(termsRelatedWith.iterator().hasNext());
+    }
+
+    /**
+     * Prove the existence in to the underlying persistence layer for a given
+     * term, the term is located after its code and code system information.
+     * This test is based on the file "startup.test.rdf".
+     * 
+     * @throws ClientException if this exception occurs then this test will fails.
+     * @throws IllegalAccessException if this exception occurs then this test will
+     * fails.
+     * @throws InstantiationException if this exception occurs then this test will
+     * fails.
+     * @throws TripleException if this exception occurs then this test will fails.
+     */
+    @Test
+    public void testGetTermURI() throws ClientException, IllegalAccessException, InstantiationException, TripleException {
+        final String codeValueStr = "C0423797";
+        // the BruisingSymptom has 
+        // code = C0423797 in the UMLS Code Ssytem
+        // the UMLS Code Ssytem has code = 2.16.840.1.113883.6.86
+        
+        final String termURI = 
+                termClient.getTermURI(codeValueStr, UMLS_CODE_SYSTEM_CODE);
+        assertNotNull(termURI);
+        
+        final DynaBean term = dynaBeanClient.getDynaBean(termURI);
+        assertNotNull(term);
+        
+        // according with the init rdf file (startup.test.rdf) the pref label for 
+        // this term is "Bruising symptom"
+        final String prefLabel = (String) term.get(Constants.SKOS_PREFLABEL);
+        assertEquals("Bruising symptom", prefLabel);
+        
+        // here I extract the code information
+        final DynaBean code = (DynaBean) term.get(Constants.CODE);
+        final String codeValue = (String) code.get(Constants.CODE_VALUE);
+        assertEquals(codeValueStr, codeValue);
+        
+        // here I extract the code system
+        final DynaBean codeSystem = (DynaBean) code.get(Constants.CODE_SYSTEM);
+        final String codeSystemCode = (String) codeSystem.get(Constants.CODE_SYSTEM_CODE);
+        assertEquals(UMLS_CODE_SYSTEM_CODE, codeSystemCode);
     }
 }
