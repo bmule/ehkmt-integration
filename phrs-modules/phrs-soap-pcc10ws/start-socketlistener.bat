90b047ab8cece9dcb490129c1974bd21ac503a90
bmule <bmulreni@salzburgresearch.at>
2012-03-12 19:36:53 +0100

    update

--- phrs-modules/phrs-soap-pcc10ws/start-socketlistener.bat
+++ phrs-modules/phrs-soap-pcc10ws/start-socketlistener.bat
@@ -1,6 +1,6 @@
 cls
 call title Start Socketlistener
-call mvn clean compile
+rem call mvn clean compile
 call mvn test-compile
 call mvn exec:java -Dexec.mainClass=at.srfg.kmt.ehealth.phrs.ws.soap.pcc10.StartSocketListener -Dexec.classpathScope=test -Djava.protocol.handler.pkgs=com.sun.net.ssl.internal.www.protocol -Djavax.net.ssl.trustStore=D:\\srdc\\codes\\icardea-google\\icardea\\icardea-caremanager-ws\\src\\test\\resources\\jssecacerts -Djavax.net.ssl.trustStorePassword=srdcpass
 
