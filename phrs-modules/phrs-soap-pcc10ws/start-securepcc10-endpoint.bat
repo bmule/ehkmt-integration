b898e89909a0cb7c9959d4b7ee1808e46e5b7f44
bmule <bmulreni@salzburgresearch.at>
2012-03-22 17:07:53 +0100

    deployment updates

--- phrs-modules/phrs-soap-pcc10ws/start-securepcc10-endpoint.bat
+++ phrs-modules/phrs-soap-pcc10ws/start-securepcc10-endpoint.bat
@@ -4,8 +4,8 @@ call title Start SECURE PCC10 Endpoint
 rem call mvn exec:java -Dexec.mainClass=at.srfg.kmt.ehealth.phrs.ws.soap.pcc10.PCC10SecureEndpointExample -Dexec.classpathScope=test -Dexec.args="icardea-server.lksdom21.lks.local 8989 testws/pcc10 srfg-phrs-core-keystore.ks icardea"
 
   if "%COMPUTERNAME%" == "SRDC-ICARDEA" (
-      echo ** SRDC machine **
-      call mvn exec:java -Dexec.mainClass=at.srfg.kmt.ehealth.phrs.ws.soap.pcc10.PCC10SecureEndpointExample  -Dexec.args="icardea-server.lksdom21.lks.local 8989 testws/pcc10 srfg-phrs-core-keystore.ks icardea"
+      echo ** SRDC machine  using 10.0.2.15 **
+      call mvn exec:java -Dexec.mainClass=at.srfg.kmt.ehealth.phrs.ws.soap.pcc10.PCC10SecureEndpointExample  -Dexec.args="10.0.2.15 8989 testws/pcc10 srfg-phrs-core-keystore.ks icardea"
 
   ) else (
       echo ** SALK machine **
