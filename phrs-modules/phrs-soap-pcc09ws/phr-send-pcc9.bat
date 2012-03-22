e1c47709e150ad08ba0a69169120b27d732fab2d
bmule <bmulreni@salzburgresearch.at>
2012-03-22 17:07:45 +0100

    deployment updates

--- phrs-modules/phrs-soap-pcc09ws/phr-send-pcc9.bat
+++ phrs-modules/phrs-soap-pcc09ws/phr-send-pcc9.bat
@@ -2,9 +2,9 @@ cls
 call title Send Medlist Cobscat pcc09
 
 if "%COMPUTERNAME%" == "SRDC-ICARDEA" (
-    echo ** SRDC ICARDEA machine **
-	call mvn exec:java -Dexec.mainClass=at.srfg.kmt.ehealth.phrs.ws.soap.pcc9.SendSecurePcc09MessageExample -Dexec.args="https://icardea-server.lksdom21.lks.local/ehrif/pcc/ https://icardea-server.lksdom21.lks.local:8989/testws/pcc10 C:\\icardea-google\\icardea\\icardea-phrs\\phrs-soap-pcc09ws\\src\\main\\resources\\srfg-phrs-core-keystore.ks icardea MEDLIST 191 Suzie Mayr F"
-    call mvn exec:java -Dexec.mainClass=at.srfg.kmt.ehealth.phrs.ws.soap.pcc9.SendSecurePcc09MessageExample -Dexec.args="https://icardea-server.lksdom21.lks.local/ehrif/pcc/ https://icardea-server.lksdom21.lks.local:8989/testws/pcc10 C:\\icardea-google\\icardea\\icardea-phrs\\phrs-soap-pcc09ws\\src\\main\\resources\\srfg-phrs-core-keystore.ks icardea COBSCAT 191 Suzie Mayr F"
+    echo ** SRDC ICARDEA machine   using 10.0.2.15 **
+	call mvn exec:java -Dexec.mainClass=at.srfg.kmt.ehealth.phrs.ws.soap.pcc9.SendSecurePcc09MessageExample -Dexec.args="https://10.0.2.15/ehrif/pcc/ https://10.0.2.15:8989/testws/pcc10 C:\\icardea-google\\icardea\\icardea-phrs\\phrs-soap-pcc09ws\\src\\main\\resources\\srfg-phrs-core-keystore.ks icardea MEDLIST 191 Suzie Mayr F"
+    call mvn exec:java -Dexec.mainClass=at.srfg.kmt.ehealth.phrs.ws.soap.pcc9.SendSecurePcc09MessageExample -Dexec.args="https://10.0.2.15/ehrif/pcc/ https://10.0.2.15:8989/testws/pcc10 C:\\icardea-google\\icardea\\icardea-phrs\\phrs-soap-pcc09ws\\src\\main\\resources\\srfg-phrs-core-keystore.ks icardea COBSCAT 191 Suzie Mayr F"
 
 )  else (
     echo ** SALK machine **
--- phrs-modules/phrs-soap-pcc09ws/start-securepcc9-endpoint.bat
+++ phrs-modules/phrs-soap-pcc09ws/start-securepcc9-endpoint.bat
@@ -1,6 +1,13 @@
 call title Start SECURE PCC09 Endpoint
 
 cls
+if "%COMPUTERNAME%" == "SRDC-ICARDEA" (
+    echo ** SRDC ICARDEA machine   using icardea-server.lksdom21.lks.local, not 10.0.2.15 **
+    call mvn exec:java -Dexec.mainClass=at.srfg.kmt.ehealth.phrs.ws.soap.pcc9.PCC9SecureEndpointExample -Dexec.args="icardea-server.lksdom21.lks.local 8089 testws/pcc9 srfg-phrs-core-keystore.ks icardea"
 
-call mvn exec:java -Dexec.mainClass=at.srfg.kmt.ehealth.phrs.ws.soap.pcc9.PCC9SecureEndpointExample -Dexec.args="icardea-server.lksdom21.lks.local 8089 testws/pcc9 srfg-phrs-core-keystore.ks icardea"
+)  else (
+    echo ** SALK machine **
+    call mvn exec:java -Dexec.mainClass=at.srfg.kmt.ehealth.phrs.ws.soap.pcc9.PCC9SecureEndpointExample -Dexec.args="icardea-server.lksdom21.lks.local 8089 testws/pcc9 srfg-phrs-core-keystore.ks icardea"
+
+)
 pause
