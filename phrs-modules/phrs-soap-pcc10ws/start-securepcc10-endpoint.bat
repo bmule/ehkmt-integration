fa6ef36395131692934c93c9a818fc0f4b436f1c
bmule <bmulreni@salzburgresearch.at>
2012-03-23 15:23:02 +0100

    Enable pcc10 processing!

--- phrs-modules/phrs-soap-pcc10ws/start-securepcc10-endpoint.bat
+++ phrs-modules/phrs-soap-pcc10ws/start-securepcc10-endpoint.bat
@@ -1,7 +1,7 @@
 cls
 call title Start SECURE PCC10 Endpoint
 
-rem call mvn exec:java -Dexec.mainClass=at.srfg.kmt.ehealth.phrs.ws.soap.pcc10.PCC10SecureEndpointExample -Dexec.classpathScope=test -Dexec.args="icardea-server.lksdom21.lks.local 8989 testws/pcc10 srfg-phrs-core-keystore.ks icardea"
+rem call mvn exec:java -Dexec.mainClass=at.srfg.kmt.ehealth.phrs.ws.soap.pcc10.PCC10SecureEndpointExample -Dexec.classpathScope=test -Dexec.args="icardea-server.lksdom21.lks.local 8989 testws/pcc10 srfg-phrs-core-keystore.ks icardea" -Dpcc10.process="true"
 
   if "%COMPUTERNAME%" == "SRDC-ICARDEA" (
       echo ** SRDC machine  using 10.0.2.15 **
