777daa186f22c1623c7a7ed1cafc24d740171607
bmule <bmulreni@salzburgresearch.at>
2012-03-23 11:24:22 +0100

    This was in the nonsecure scripts, but missing on the secure scripts, failed the deployments: -Dpcc10.process="true"

--- phrs-modules/phrs-soap-pcc10ws/start-securepcc10-endpoint.bash
+++ phrs-modules/phrs-soap-pcc10ws/start-securepcc10-endpoint.bash
@@ -5,6 +5,6 @@ clear
 
 rem mvn clean compile
 rem mvn test-compile
-mvn exec:java -Dexec.mainClass=at.srfg.kmt.ehealth.phrs.ws.soap.pcc10.PCC10SecureEndpointExample  -Dexec.args="localhost 8989 testws/pcc10 srfg-phrs-core-keystore.ks icardea"
+mvn exec:java -Dexec.mainClass=at.srfg.kmt.ehealth.phrs.ws.soap.pcc10.PCC10SecureEndpointExample  -Dexec.args="localhost 8989 testws/pcc10 srfg-phrs-core-keystore.ks icardea" -Dpcc10.process="true"
 
 
--- phrs-modules/phrs-soap-pcc10ws/start-securepcc10-endpoint.bat
+++ phrs-modules/phrs-soap-pcc10ws/start-securepcc10-endpoint.bat
@@ -5,11 +5,11 @@ rem call mvn exec:java -Dexec.mainClass=at.srfg.kmt.ehealth.phrs.ws.soap.pcc10.P
 
   if "%COMPUTERNAME%" == "SRDC-ICARDEA" (
       echo ** SRDC machine  using 10.0.2.15 **
-      call mvn exec:java -Dexec.mainClass=at.srfg.kmt.ehealth.phrs.ws.soap.pcc10.PCC10SecureEndpointExample  -Dexec.args="10.0.2.15 8989 testws/pcc10 srfg-phrs-core-keystore.ks icardea"
+      call mvn exec:java -Dexec.mainClass=at.srfg.kmt.ehealth.phrs.ws.soap.pcc10.PCC10SecureEndpointExample  -Dexec.args="10.0.2.15 8989 testws/pcc10 srfg-phrs-core-keystore.ks icardea" -Dpcc10.process="true"
 
   ) else (
       echo ** SALK machine **
-      call mvn exec:java -Dexec.mainClass=at.srfg.kmt.ehealth.phrs.ws.soap.pcc10.PCC10SecureEndpointExample  -Dexec.args="icardea-server.lksdom21.lks.local 8989 testws/pcc10 srfg-phrs-core-keystore.ks icardea"
+      call mvn exec:java -Dexec.mainClass=at.srfg.kmt.ehealth.phrs.ws.soap.pcc10.PCC10SecureEndpointExample  -Dexec.args="icardea-server.lksdom21.lks.local 8989 testws/pcc10 srfg-phrs-core-keystore.ks icardea" -Dpcc10.process="true"
 
   )
 pause
